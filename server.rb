e1323ad911fd171a8f5b71135e9b9359237a6c55
Matthew Landauer <matthew@openaustralia.org>
2013-03-30 18:43:43 +1100

    Extract processing of mail into new class

--- server.rb
+++ server.rb
@@ -9,9 +9,20 @@ class StdoutSmtpServer < MiniSmtpServer
 
   def new_message_event(message_hash)
     # TODO: Put the handler job into a delayed job queue
-
     # For the time being just do it here to keep things super simple
+    MailJob.new(message_hash).perform
+  end
 
+end
+
+class MailJob
+  attr_reader :message_hash
+
+  def initialize(message_hash)
+    @message_hash = message_hash
+  end
+
+  def perform
     puts "# New email received:"
     puts "-- From: #{message_hash[:from]}"
     puts "-- To:   #{message_hash[:to]}"
@@ -19,13 +30,17 @@ class StdoutSmtpServer < MiniSmtpServer
     puts "-- " + message_hash[:data].gsub(/\r\n/, "\r\n-- ")
     puts
 
-    # Simple pass the message to a local SMTP server (mailcatcher for the time being)
-    Net::SMTP.start('localhost', 1025) do |smtp|
-      # Use the SMTP object smtp only in this block.
-      smtp.send_message(message_hash[:data], message_hash[:from], message_hash[:to])
-    end
+    forward('localhost', 1025)
   end
 
+  # Send this mail to another smtp server
+  def forward(server, port)
+    # Simple pass the message to a local SMTP server (mailcatcher for the time being)
+    Net::SMTP.start(server, port) do |smtp|
+      # Use the SMTP object smtp only in this block.
+      smtp.send_message(message_hash[:data], message_hash[:from], message_hash[:to])
+    end    
+  end
 end
 
 # Create a new server instance listening at 127.0.0.1:2525
