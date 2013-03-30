3a03c5b03022a2eed095dd84aea9618b9acb0a3d
Matthew Landauer <matthew@openaustralia.org>
2013-03-31 09:28:37 +1100

    Add EmailAddress model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130330193042) do
+ActiveRecord::Schema.define(version: 20130330222744) do
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20130330193042) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+  create_table "email_addresses", force: true do |t|
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "emails", force: true do |t|
     t.string   "from"
