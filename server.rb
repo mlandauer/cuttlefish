c54147475be5bf7a205d489edc06b5aa80aebd8f
Matthew Landauer <matthew@openaustralia.org>
2013-03-31 05:21:33 +1100

    Load database config from standard rails place

--- server.rb
+++ server.rb
@@ -15,10 +15,10 @@ class StdoutSmtpServer < MiniSmtpServer
 
 end
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database  => "db/development.sqlite3"
-)
+activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), 'config', 'database.yml')))
+
+# Hardcoded to the development environment for the time being
+ActiveRecord::Base.establish_connection(activerecord_config["development"])
 
 # Create a new server instance listening at 127.0.0.1:2525
 # and accepting a maximum of 4 simultaneous connections
@@ -26,5 +26,4 @@ server = StdoutSmtpServer.new(2525, "127.0.0.1", 4)
 
 # Start the server
 server.start
-
 server.join
