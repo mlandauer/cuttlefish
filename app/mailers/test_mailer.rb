73987f8152e6c541ecaf822005eef0ccae230c76
Matthew Landauer <matthew@openaustralia.org>
2013-03-31 09:26:48 +1100

    Pass in a full name in the test email

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -8,7 +8,7 @@ class TestMailer < ActionMailer::Base
   #
   def test
     mail subject: "This is a test email from Cuttlefish",
-      to: ["matthew@openaustralia.org", "mlandauer@yahoo.com"],
+      to: ["Matthew Landauer <matthew@openaustralia.org>", "mlandauer@yahoo.com"],
       cc: "kat@openaustralia.org",
       bcc: "matthew@planningalerts.org.au"
   end
