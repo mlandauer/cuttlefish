6cbd989ac55baf5446d17aa8acd00c57d095ac0e
Matthew Landauer <matthew@openaustralia.org>
2013-03-31 05:30:08 +1100

    Now tells the user what is happening

--- server.rb
+++ server.rb
@@ -1,18 +1,14 @@
-#!/usr/bin/env ruby -d
+#!/usr/bin/env ruby
 
 require 'mini-smtp-server'
 require 'net/smtp'
 require 'delayed_job_active_record'
 require File.join(File.dirname(__FILE__), 'lib', 'mail_job')
 
-# This is an SMTP server that logs all
-# the messages it receives to STDOUT
-class StdoutSmtpServer < MiniSmtpServer
-
+class CuttlefishSmtpServer < MiniSmtpServer
   def new_message_event(message_hash)
     Delayed::Job.enqueue MailJob.new(message_hash)
   end
-
 end
 
 activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), 'config', 'database.yml')))
@@ -20,10 +16,18 @@ activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), 'con
 # Hardcoded to the development environment for the time being
 ActiveRecord::Base.establish_connection(activerecord_config["development"])
 
+host = "127.0.0.1"
+port = 2525
+
 # Create a new server instance listening at 127.0.0.1:2525
 # and accepting a maximum of 4 simultaneous connections
-server = StdoutSmtpServer.new(2525, "127.0.0.1", 4)
+server = CuttlefishSmtpServer.new(port, host, 4)
 
 # Start the server
 server.start
+
+puts "My eight arms and two tentacles are quivering in anticipation."
+puts "I'm listening for emails via SMTP on #{host} port #{port}" 
+
 server.join
+
