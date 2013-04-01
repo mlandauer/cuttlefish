f6e8665103b964e28bb88ac747bc521b92e4374c
Matthew Landauer <matthew@openaustralia.org>
2013-04-01 12:34:57 +1100

    Extract common stuff in tests

--- spec/lib/raw_email_spec.rb
+++ spec/lib/raw_email_spec.rb
@@ -1,15 +1,22 @@
 require "spec_helper"
 
 describe RawEmail do
-  it "should record the email in the database" do
-    email = RawEmail.new("matthew@foo.org", ["mlandauer@foo.org", "matthew@bar.com"], "This the main bit of the email")
-    email.record
+  describe ".record" do
+    before :each do
+      email = RawEmail.new("matthew@foo.org", ["mlandauer@foo.org", "matthew@bar.com"], "This the main bit of the email")
+      email.record
+    end
+    let(:saved) { Email.first }
 
-    saved = Email.first
-    saved.from.should == "matthew@foo.org"
-    a1 = EmailAddress.find_by_address("matthew@foo.org")
-    a1.should_not be_nil
-    saved.from_address.should == a1
-    saved.to.should == "mlandauer@foo.org, matthew@bar.com"
+    it "should record from address in the database" do
+      saved.from.should == "matthew@foo.org"
+      a1 = EmailAddress.find_by_address("matthew@foo.org")
+      a1.should_not be_nil
+      saved.from_address.should == a1
+    end
+
+    it "should record the to addresses in the database" do
+      saved.to.should == "mlandauer@foo.org, matthew@bar.com"
+    end
   end
 end
