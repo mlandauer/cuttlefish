157da8ec7e5944da2fd45687e86da6f2c9715a9e
Matthew Landauer <matthew@openaustralia.org>
2013-04-01 18:43:08 +1100

    Add test for EmailAddress#emails_sent

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".emails_sent" do
+    it "should be able to find all the emails sent to this address" do
+      email1 = Email.create!(:from => "matthew@foo.com")
+      email2 = Email.create!(:from => "matthew@foo.com")
+      email3 = Email.create!(:from => "peter@bar.com")
+
+      EmailAddress.find_by_address("matthew@foo.com").emails_sent.should == [email1, email2]
+    end
+  end
 end
