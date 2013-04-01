6427a49082c10e09e7d5ff75fc01b9063af1e65a
Matthew Landauer <matthew@openaustralia.org>
2013-04-01 14:55:49 +1100

    Moved tests in lib/raw_email_spec to models/email_spec

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -13,12 +13,32 @@ describe Email do
     end
   end
 
+  describe ".from_address" do
+    it "should return an EmailAddress object" do
+      email = Email.create!(from: "matthew@foo.org")
+      a1 = EmailAddress.find_by_address("matthew@foo.org")
+      a1.should_not be_nil
+      email.from_address.should == a1
+    end
+  end
+
   describe ".to" do
     it "should return an array for all the email addresses" do
       Email.create!(:to => ["mlandauer@foo.org", "matthew@bar.com"])
 
       email = Email.find_by_to("mlandauer@foo.org, matthew@bar.com")
       email.to.should == ["mlandauer@foo.org", "matthew@bar.com"]
+    end
+  end
+
+  describe ".to_addresses" do
+    it "should return an array of EmailAddress objects" do
+      email = Email.create!(to: ["mlandauer@foo.org", "matthew@bar.com"])
+      a1 = EmailAddress.find_by_address("mlandauer@foo.org")
+      a2 = EmailAddress.find_by_address("matthew@bar.com")
+      a1.should_not be_nil
+      a2.should_not be_nil
+      email.to_addresses.should == [a1, a2]
     end
   end
 end
