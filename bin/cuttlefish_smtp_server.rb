d94e97d217b9c628e966fdccec8e9cce7735993b
Matthew Landauer <matthew@openaustralia.org>
2013-04-03 17:45:36 +1100

    Move server start code into module

--- bin/cuttlefish_smtp_server.rb
+++ bin/cuttlefish_smtp_server.rb
@@ -1,28 +1,33 @@
 #!/usr/bin/env ruby
 # If you need a bit of debugging output in the threads add -d to the line above
 
-# For the benefit of foreman
-$stdout.sync = true
-
 $: << File.join(File.dirname(__FILE__), "..", "lib")
 
 require 'cuttlefish_smtp_server'
 
-# Hardcoded to the development environment for the time being
-environment = "development"
-host = "127.0.0.1"
-port = 2525
-number_of_connections = 4
+module CuttlefishControl
+  def self.smtp_start
+    # Hardcoded to the development environment for the time being
+    environment = "development"
+    host = "127.0.0.1"
+    port = 2525
+    number_of_connections = 4
 
-activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml')))
-ActiveRecord::Base.establish_connection(activerecord_config[environment])
+    # For the benefit of foreman
+    $stdout.sync = true
 
-server = CuttlefishSmtpServer.new(port, host, number_of_connections)
-server.audit = true
-server.start
+    activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml')))
+    ActiveRecord::Base.establish_connection(activerecord_config[environment])
 
-puts "My eight arms and two tentacles are quivering in anticipation."
-puts "I'm listening for emails via SMTP on #{host} port #{port}" 
+    server = CuttlefishSmtpServer.new(port, host, number_of_connections)
+    server.audit = true
+    server.start
 
-server.join
+    puts "My eight arms and two tentacles are quivering in anticipation."
+    puts "I'm listening for emails via SMTP on #{host} port #{port}" 
 
+    server.join
+  end
+end
+
+CuttlefishControl.smtp_start
