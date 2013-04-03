8a79f8b571f5eecde0dd9a6808bad96738725cb2
Matthew Landauer <matthew@openaustralia.org>
2013-04-03 17:47:20 +1100

    Move module to its own file

--- bin/cuttlefish_smtp_server.rb
+++ bin/cuttlefish_smtp_server.rb
@@ -3,31 +3,6 @@
 
 $: << File.join(File.dirname(__FILE__), "..", "lib")
 
-require 'cuttlefish_smtp_server'
-
-module CuttlefishControl
-  def self.smtp_start
-    # Hardcoded to the development environment for the time being
-    environment = "development"
-    host = "127.0.0.1"
-    port = 2525
-    number_of_connections = 4
-
-    # For the benefit of foreman
-    $stdout.sync = true
-
-    activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml')))
-    ActiveRecord::Base.establish_connection(activerecord_config[environment])
-
-    server = CuttlefishSmtpServer.new(port, host, number_of_connections)
-    server.audit = true
-    server.start
-
-    puts "My eight arms and two tentacles are quivering in anticipation."
-    puts "I'm listening for emails via SMTP on #{host} port #{port}" 
-
-    server.join
-  end
-end
+require 'cuttlefish_control'
 
 CuttlefishControl.smtp_start
---
+++ lib/cuttlefish_control.rb
@@ -0,0 +1,26 @@
+require 'cuttlefish_smtp_server'
+
+module CuttlefishControl
+  def self.smtp_start
+    # Hardcoded to the development environment for the time being
+    environment = "development"
+    host = "127.0.0.1"
+    port = 2525
+    number_of_connections = 4
+
+    # For the benefit of foreman
+    $stdout.sync = true
+
+    activerecord_config = YAML.load(File.read(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml')))
+    ActiveRecord::Base.establish_connection(activerecord_config[environment])
+
+    server = CuttlefishSmtpServer.new(port, host, number_of_connections)
+    server.audit = true
+    server.start
+
+    puts "My eight arms and two tentacles are quivering in anticipation."
+    puts "I'm listening for emails via SMTP on #{host} port #{port}" 
+
+    server.join
+  end
+end
