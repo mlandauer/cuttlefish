471dd47928c9257dc6fd87fce3fabbfccec05d90
Matthew Landauer <matthew@openaustralia.org>
2013-04-06 06:50:58 +1100

    Move method to different class

--- app/models/postfix_log_line.rb
+++ app/models/postfix_log_line.rb
@@ -1,3 +1,18 @@
 class PostfixLogLine < ActiveRecord::Base
   belongs_to :email
+
+  def self.create_from_line(line)
+    queue_id = PostfixLog.extract_postfix_queue_id_from_line(line)
+    time = PostfixLog.extract_time_from_postfix_log_line(line)
+    text = PostfixLog.extract_main_content_postfix_log_line(line)
+
+    # TODO: Should find the most recent email with the queue ID (as there may be several)
+    email = Email.find_by_postfix_queue_id(queue_id)
+    if email
+      # Don't resave duplicates
+      find_or_create_by(time: time, text: text, email: email)
+    else
+      puts "Skipping postfix queue id #{queue_id} - it's not recognised"
+    end
+  end
 end
--- lib/postfix_log.rb
+++ lib/postfix_log.rb
@@ -10,7 +10,7 @@ module PostfixLog
     while true
       if File.exists?(file)
         File::Tail::Logfile.open(file) do |log|
-          log.tail { |line| process(line) }
+          log.tail { |line| PostfixLogLine.create_from_line(line) }
         end
       else
         sleep(10)
@@ -27,24 +27,11 @@ module PostfixLog
     parse_postfix_log_line(line).time
   end
 
-  def self.process(line)
-    queue_id = extract_postfix_queue_id_from_line(line)
-    # TODO: Should find the most recent email with the queue ID (as there may be several)
-    email = Email.find_by_postfix_queue_id(queue_id)
-    if email
-      # Don't resave duplicates
-      email.postfix_log_lines.find_or_create_by(time: extract_time_from_postfix_log_line(line),
-        text: extract_main_content_postfix_log_line(line))
-    else
-      puts "Skipping postfix queue id #{queue_id} - it's not recognised"
-    end
-  end
-
-  private
-
   def self.extract_main_content_postfix_log_line(line)
     parse_postfix_log_line(line).content
   end
+
+  private
 
   def self.parse_postfix_log_line(line)
     # Assume the log file was written using syslog and parse accordingly
