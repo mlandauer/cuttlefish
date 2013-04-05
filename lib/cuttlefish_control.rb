e2faccf520cede4df08401b61a5e25204e5b0436
Matthew Landauer <matthew@openaustralia.org>
2013-04-06 07:07:47 +1100

    Move main loop of log sucker to CuttlefishControl module

--- lib/cuttlefish_control.rb
+++ lib/cuttlefish_control.rb
@@ -23,4 +23,21 @@ module CuttlefishControl
 
     server.join
   end
+
+  def self.log_start
+    # For the benefit of foreman
+    $stdout.sync = true
+
+    file = "/var/log/mail/mail.log"
+    puts "Sucking up log entries in #{file}..."
+    while true
+      if File.exists?(file)
+        File::Tail::Logfile.open(file) do |log|
+          log.tail { |line| PostfixLogLine.create_from_line(line) }
+        end
+      else
+        sleep(10)
+      end
+    end
+  end
 end
--- lib/tasks/cuttlefish.rake
+++ lib/tasks/cuttlefish.rake
@@ -1,5 +1,4 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "cuttlefish_control"))
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "postfix_log"))
 
 namespace :cuttlefish do
   desc "Start the Cuttlefish SMTP server"
@@ -9,6 +8,6 @@ namespace :cuttlefish do
 
   desc "Start the Postfix mail log sucker"
   task :log => :environment do
-    PostfixLog::tail
+    CuttlefishControl.log_start
   end
 end
