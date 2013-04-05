a69dfce9e427365aaf6272255622d8f8c63b0efe
Matthew Landauer <matthew@openaustralia.org>
2013-04-05 19:12:49 +1100

    Extract method

--- lib/postfix_log.rb
+++ lib/postfix_log.rb
@@ -19,18 +19,24 @@ module PostfixLog
   end
 
   def self.extract_postfix_queue_id_from_line(line)
-    # Assume the log file was written using syslog and parse accordingly
-    m = SyslogProtocol.parse("<13>" + line).content.match(/^\S+: (\S+):/)
+    m = parse_postfix_log_line(line).content.match(/^\S+: (\S+):/)
     m[1] if m
   end
 
   def self.extract_time_from_postfix_log_line(line)
-    SyslogProtocol.parse("<13>" + line).time
+    parse_postfix_log_line(line).time
   end
 
   def self.process(line)
     postfix_queue_id = extract_postfix_queue_id_from_line(line)
     email = Email.find_by_postfix_queue_id(postfix_queue_id)
     email.postfix_log_lines.create(text: line)
+  end
+
+  private
+
+  def self.parse_postfix_log_line(line)
+    # Assume the log file was written using syslog and parse accordingly
+    SyslogProtocol.parse("<13>" + line)
   end
 end
