495763e76156e31b4cace146a314612cc01c1d9c
Matthew Landauer <matthew@openaustralia.org>
2013-04-06 11:12:42 +1100

    Inline methods

--- app/models/postfix_log_line.rb
+++ app/models/postfix_log_line.rb
@@ -2,36 +2,19 @@ class PostfixLogLine < ActiveRecord::Base
   belongs_to :email
 
   def self.create_from_line(line)
+    values = match_main_content(line)
+
     # Only log delivery attempts
-    if program(line) == "smtp"
-      queue_id = queue_id(line)
-      time = time(line)
-      text = program_content(line)
+    if values[:program] == "smtp"
       # TODO: Should find the most recent email with the queue ID (as there may be several)
-      email = Email.find_by_postfix_queue_id(queue_id)
+      email = Email.find_by_postfix_queue_id(values[:queue_id])
       if email
         # Don't resave duplicates
-        find_or_create_by(time: time, text: text, email: email)
+        find_or_create_by(time: values[:time], text: values[:program_content], email: email)
       else
-        puts "Skipping postfix queue id #{queue_id} - it's not recognised"
+        puts "Skipping postfix queue id #{values[:queue_id]} - it's not recognised"
       end
     end
-  end
-
-  def self.queue_id(line)
-    match_main_content(line)[:queue_id]
-  end
-
-  def self.time(line)
-    match_main_content(line)[:time]
-  end
-
-  def self.program(line)
-    match_main_content(line)[:program]
-  end
-
-  def self.program_content(line)
-    match_main_content(line)[:program_content]
   end
 
   def self.match_main_content(line)
