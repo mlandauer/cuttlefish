1aafb07f34dbbb1753872e417f3acb73fc30a04e
Matthew Landauer <matthew@openaustralia.org>
2013-04-06 13:25:22 +1100

    Look at all the emails that a user has received or sent

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,7 +1,15 @@
 class EmailsController < ApplicationController
 
   def index
-    @emails = Email.order("created_at DESC").paginate(:page => params[:page])
+    if params[:from]
+      @email_address = EmailAddress.find_by_address(params[:from])
+      @emails = @email_address.emails_sent.order("created_at DESC").paginate(:page => params[:page])
+    elsif params[:to]
+      @email_address = EmailAddress.find_by_address(params[:to])
+      @emails = @email_address.emails_received.order("created_at DESC").paginate(:page => params[:page])
+    else
+      @emails = Email.order("created_at DESC").paginate(:page => params[:page])
+    end
   end
 
   def show
