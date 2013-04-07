c00e08600d3100705563b65eeeccb35a8f293ba3
Matthew Landauer <matthew@openaustralia.org>
2013-04-07 14:29:11 +1000

    Inline method

--- app/models/postfix_log_line.rb
+++ app/models/postfix_log_line.rb
@@ -5,23 +5,6 @@ class PostfixLogLine < ActiveRecord::Base
     dsn == "2.0.0"
   end
 
-  def self.extract_main_content_info(program_content)
-    to_match = program_content.match(/to=<([^>]+)>/)
-    relay_match = program_content.match(/relay=([^,]+)/)
-    delay_match = program_content.match(/delay=([^,]+)/)
-    delays_match = program_content.match(/delays=([^,]+)/)
-    dsn_match = program_content.match(/dsn=([^,]+)/)
-    status_match = program_content.match(/status=(.*)$/)
-    result = {}
-    result[:to] = to_match[1] if to_match
-    result[:relay] = relay_match[1] if relay_match
-    result[:delay] = delay_match[1] if delay_match
-    result[:delays] = delays_match[1] if delays_match
-    result[:dsn] = dsn_match[1] if dsn_match
-    result[:status] = status_match[1] if status_match
-    result
-  end
-
   def self.create_from_line(line)
     values = match_main_content(line)
 
@@ -45,8 +28,30 @@ class PostfixLogLine < ActiveRecord::Base
   def self.match_main_content(line)
     # Assume the log file was written using syslog and parse accordingly
     p = SyslogProtocol.parse("<13>" + line)
-    m = p.content.match /^postfix\/(\w+)\[(\d+)\]: (([0-9A-F]+): )?(.*)/
-    {:time => p.time, :program => m[1], :pid => m[2], :queue_id => m[4], :program_content => m[5]}.merge(extract_main_content_info(m[5]))
+    content_match = p.content.match /^postfix\/(\w+)\[(\d+)\]: (([0-9A-F]+): )?(.*)/
+    program_content = content_match[5]
+    to_match = program_content.match(/to=<([^>]+)>/)
+    relay_match = program_content.match(/relay=([^,]+)/)
+    delay_match = program_content.match(/delay=([^,]+)/)
+    delays_match = program_content.match(/delays=([^,]+)/)
+    dsn_match = program_content.match(/dsn=([^,]+)/)
+    status_match = program_content.match(/status=(.*)$/)
+
+    result = {
+      :time => p.time,
+      :program => content_match[1],
+      :pid => content_match[2],
+      :queue_id => content_match[4],
+      :program_content => program_content
+    }
+    result[:to] = to_match[1] if to_match
+    result[:relay] = relay_match[1] if relay_match
+    result[:delay] = delay_match[1] if delay_match
+    result[:delays] = delays_match[1] if delays_match
+    result[:dsn] = dsn_match[1] if dsn_match
+    result[:status] = status_match[1] if status_match
+
+    result
   end
-  
+
 end
