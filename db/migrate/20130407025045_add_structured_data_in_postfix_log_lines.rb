66bc70540c9c2e6c8d8472de261c8ae802fb4a4e
Matthew Landauer <matthew@openaustralia.org>
2013-04-07 16:54:16 +1000

    Handle non matches correctly

--- db/migrate/20130407025045_add_structured_data_in_postfix_log_lines.rb
+++ db/migrate/20130407025045_add_structured_data_in_postfix_log_lines.rb
@@ -8,13 +8,19 @@ class AddStructuredDataInPostfixLogLines < ActiveRecord::Migration
     add_column :postfix_log_lines, :status, :text, :limit => nil
     PostfixLogLine.reset_column_information
     PostfixLogLine.all.each do |l|
+      to_match = l.text.match(/to=<([^>]+)>/)
+      relay_match = l.text.match(/relay=([^,]+)/)
+      delay_match = l.text.match(/delay=([^,]+)/)
+      delays_match = l.text.match(/delays=([^,]+)/)
+      dsn_match = l.text.match(/dsn=([^,]+)/)
+      status_match = l.text.match(/status=(.*)$/)
       l.update_attributes!(
-        to: l.text.match(/to=<([^>]+)>/)[1],
-        relay: l.text.match(/relay=([^,]+)/)[1],
-        delay: l.text.match(/delay=([^,]+)/)[1],
-        delays: l.text.match(/delays=([^,]+)/)[1],
-        dsn: l.text.match(/dsn=([^,]+)/)[1],
-        status: l.text.match(/status=(.*)$/)[1]
+        to: (to_match[1] if to_match),
+        relay: (relay_match[1] if relay_match),
+        delay: (delay_match[1] if delay_match),
+        delays: (delays_match[1] if delays_match),
+        dsn: (dsn_match[1] if dsn_match),
+        status: (status_match[1] if status_match)
       )
     end
   end
