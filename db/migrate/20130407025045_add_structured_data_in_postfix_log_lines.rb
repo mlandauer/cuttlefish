e19527dfd25b0015eb030aa83ea0b11a3b0d7e89
Matthew Landauer <matthew@openaustralia.org>
2013-04-07 16:43:39 +1000

    Inline method that has now been deleted

--- db/migrate/20130407025045_add_structured_data_in_postfix_log_lines.rb
+++ db/migrate/20130407025045_add_structured_data_in_postfix_log_lines.rb
@@ -8,7 +8,14 @@ class AddStructuredDataInPostfixLogLines < ActiveRecord::Migration
     add_column :postfix_log_lines, :status, :text, :limit => nil
     PostfixLogLine.reset_column_information
     PostfixLogLine.all.each do |l|
-      l.update_attributes!(l.main_content_info)
+      l.update_attributes!(
+        to: text.match(/to=<([^>]+)>/)[1],
+        relay: text.match(/relay=([^,]+)/)[1],
+        delay: text.match(/delay=([^,]+)/)[1],
+        delays: text.match(/delays=([^,]+)/)[1],
+        dsn: text.match(/dsn=([^,]+)/)[1],
+        status: text.match(/status=(.*)$/)[1]
+      )
     end
   end
 end
