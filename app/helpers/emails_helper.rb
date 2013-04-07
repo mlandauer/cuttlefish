1e25e204fc4351de26e1607c3a18234dbe61cc93
Matthew Landauer <matthew@openaustralia.org>
2013-04-07 17:18:31 +1000

    Make colouring more consistent

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,2 +1,11 @@
 module EmailsHelper
+  # Give a warning level (used for colouring things in Bootstrap) based on whether the email has
+  # been delivered succesfully
+  def delivered_class(email)
+    if email.delivered == true
+      "success"
+    elsif email.delivered == false
+      "warning"
+    end
+  end
 end
