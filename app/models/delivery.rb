5971eacdcd4d7b4d636440119ffd59df0c0f47e2
Matthew Landauer <matthew@openaustralia.org>
2013-04-10 18:16:25 +1000

    Refactor old methods to use new methods

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,14 +4,13 @@ class Delivery < ActiveRecord::Base
   has_many :postfix_log_lines, -> { order "time DESC" }
 
   def delivered
-    unless postfix_log_lines.empty?
-      # Take the delivery status from the last delivery attempt
-      postfix_log_lines.first.delivered?
+    if status != "unknown"
+      status == "delivered"
     end
   end
 
   def delivered_status_known?
-    !delivered.nil?
+    status != "unknown"
   end
 
   def status
