08fd570b6907ef01b5eeef4ef7caafa689aa7da5
Matthew Landauer <matthew@openaustralia.org>
2013-04-10 13:03:20 +1000

    Migrate the data as well

--- db/migrate/20130410021031_add_delivery_id_to_postfix_log_lines.rb
+++ db/migrate/20130410021031_add_delivery_id_to_postfix_log_lines.rb
@@ -1,5 +1,12 @@
 class AddDeliveryIdToPostfixLogLines < ActiveRecord::Migration
   def change
     add_column :postfix_log_lines, :delivery_id, :integer
+
+    PostfixLogLine.reset_column_information
+    PostfixLogLine.all.each do |line|
+      address = Address.find_by_text(line.to)
+      delivery = line.email.deliveries.find_by_address_id(address.id) if address
+      line.update_attribute(:delivery_id, delivery.id) if delivery
+    end
   end
 end
