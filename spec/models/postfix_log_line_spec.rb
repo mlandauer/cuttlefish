fb6f3a9c423bf5471821ca4a535118d016748146
Matthew Landauer <matthew@openaustralia.org>
2013-04-10 11:35:46 +1000

    Test that PostfixLogLine.create_from_line also attaches log lines to the Delivery model

--- spec/models/postfix_log_line_spec.rb
+++ spec/models/postfix_log_line_spec.rb
@@ -34,13 +34,26 @@ describe PostfixLogLine do
   end
 
   describe ".create_from_line" do
-    it "should extract and save relevant parts of the line" do
-      email = Email.create!(postfix_queue_id: "39D9336AFA81")
-      PostfixLogLine.create_from_line(line1)
-      PostfixLogLine.count.should == 1
-      line = email.postfix_log_lines.first
-      line.text.should == "to=<foo@bar.com>, relay=foo.bar.com[1.2.3.4]:25, delay=92780, delays=92777/0.03/1.6/0.91, dsn=4.3.0, status=deferred (host foo.bar.com[1.2.3.4] said: 451 4.3.0 <bounces@planningalerts.org.au>: Temporary lookup failure (in reply to RCPT TO command))"
-      line.time.should == Time.local(2013,4,5,16,41,54)
+    context "one log line" do
+      let(:email) { Email.create!(postfix_queue_id: "39D9336AFA81", to: "foo@bar.com") }
+      before :each do
+        email
+        PostfixLogLine.create_from_line(line1)
+      end
+
+      it "should extract and save relevant parts of the line" do
+        PostfixLogLine.count.should == 1
+        line = email.postfix_log_lines.first
+        line.text.should == "to=<foo@bar.com>, relay=foo.bar.com[1.2.3.4]:25, delay=92780, delays=92777/0.03/1.6/0.91, dsn=4.3.0, status=deferred (host foo.bar.com[1.2.3.4] said: 451 4.3.0 <bounces@planningalerts.org.au>: Temporary lookup failure (in reply to RCPT TO command))"
+        line.time.should == Time.local(2013,4,5,16,41,54)
+      end
+
+      it "should attach it to the delivery" do
+        email.deliveries.first.postfix_log_lines.count.should == 1
+        line = email.deliveries.first.postfix_log_lines.first
+        line.text.should == "to=<foo@bar.com>, relay=foo.bar.com[1.2.3.4]:25, delay=92780, delays=92777/0.03/1.6/0.91, dsn=4.3.0, status=deferred (host foo.bar.com[1.2.3.4] said: 451 4.3.0 <bounces@planningalerts.org.au>: Temporary lookup failure (in reply to RCPT TO command))"
+        line.time.should == Time.local(2013,4,5,16,41,54)
+      end
     end
 
     it "should save two lines if two lines are processed" do
