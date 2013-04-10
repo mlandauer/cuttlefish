2238a80675c5c2f1b12a21a1fff2b6d5c456a313
Matthew Landauer <matthew@openaustralia.org>
2013-04-10 15:03:22 +1000

    Quick and hacky filter by status

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,12 @@
 class EmailsController < ApplicationController
 
   def index
+    if params[:delivered] == "true"
+      @delivered = true
+    elsif params[:delivered] == "false"
+      @delivered = false
+    end
+
     if params[:from]
       @address = Address.find_by_text(params[:from])
       @emails = @address.emails_sent.order("created_at DESC").paginate(:page => params[:page])
@@ -8,7 +14,11 @@ class EmailsController < ApplicationController
       @address = Address.find_by_text(params[:to])
       @emails = @address.emails_received.order("created_at DESC").paginate(:page => params[:page])
     else
-      @emails = Email.order("created_at DESC").paginate(:page => params[:page])
+      if @delivered.nil?
+        @emails = Email.order("created_at DESC").paginate(:page => params[:page])
+      else
+        @emails = Email.where(delivered: @delivered).order("created_at DESC").paginate(:page => params[:page])
+      end
     end
   end
 
