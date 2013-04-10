649864e908ad250a75003b98ee2fb1bbd7660e94
Matthew Landauer <matthew@openaustralia.org>
2013-04-10 17:25:24 +1000

    From email show page now link to address show page

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -7,18 +7,10 @@ class EmailsController < ApplicationController
       @delivered = false
     end
 
-    if params[:from]
-      @address = Address.find_by_text(params[:from])
-      @emails = @address.emails_sent.order("created_at DESC").paginate(:page => params[:page])
-    elsif params[:to]
-      @address = Address.find_by_text(params[:to])
-      @emails = @address.emails_received.order("created_at DESC").paginate(:page => params[:page])
+    if @delivered.nil?
+      @emails = Email.order("created_at DESC").paginate(:page => params[:page])
     else
-      if @delivered.nil?
-        @emails = Email.order("created_at DESC").paginate(:page => params[:page])
-      else
-        @emails = Email.where(delivered: @delivered).order("created_at DESC").paginate(:page => params[:page])
-      end
+      @emails = Email.where(delivered: @delivered).order("created_at DESC").paginate(:page => params[:page])
     end
   end
 
