bcc56752abc6d43daa1948aa4c23c2ec07ec998b
Matthew Landauer <matthew@openaustralia.org>
2013-04-10 16:24:57 +1000

    Refactor tests a bit

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,23 +1,33 @@
 require 'spec_helper'
 
 describe Address do
-  describe "#emails_sent" do
-    it "should be able to find all the emails sent from this address" do
-      email1 = Email.create!(:from => "matthew@foo.com")
-      email2 = Email.create!(:from => "matthew@foo.com")
-      email3 = Email.create!(:from => "peter@bar.com")
-
-      Address.find_by_text("matthew@foo.com").emails_sent.should == [email1, email2]
+  context "three emails" do
+    let (:address1) { Address.create!(text: "matthew@foo.com") }
+    let (:address2) { Address.create!(text: "peter@bar.com") }
+    before :each do
+      @email1 = Email.create!(from_address: address1, to_addresses: [address1])
+      @email2 = Email.create!(from_address: address1, to_addresses: [address2])
+      @email3 = Email.create!(from_address: address2, to_addresses: [address2])
     end
-  end
 
-  describe "#emails_received" do
-    it "should be able to find all the emails received by this address" do
-      email1 = Email.create!(:to => "matthew@foo.com")
-      email2 = Email.create!(:to => "matthew@foo.com")
-      email3 = Email.create!(:to => "peter@bar.com")
+    describe "#emails_sent" do
+      it "should be able to find all the emails sent from this address" do
+        address1.emails_sent.should == [@email1, @email2]
+      end
 
-      Address.find_by_text("matthew@foo.com").emails_received.should == [email1, email2]
+      it "should be able to find all the emails sent from this address" do
+        address2.emails_sent.should == [@email3]
+      end
+    end
+
+    describe "#emails_received" do
+      it "should be able to find all the emails received by this address" do
+        address1.emails_received.should == [@email1]
+      end
+
+      it "should be able to find all the emails received by this address" do
+        address2.emails_received.should == [@email2, @email3]
+      end
     end
   end
 end
