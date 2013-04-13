9b2d403f2e6febcb7d82113415a98d338ace40dc
Matthew Landauer <matthew@openaustralia.org>
2013-04-13 13:48:03 +1000

    Refactor tests

--- spec/lib/outgoing_email_spec.rb
+++ spec/lib/outgoing_email_spec.rb
@@ -4,75 +4,71 @@ describe OutgoingEmail do
   describe "#send" do
     context "an email with one recipient" do
       before :each do
-        @email = Email.create!(:to => "foo@bar.com")
+        @email = Email.create!(:to => "foo@bar.com", :data => "My original data")
+        @outgoing = OutgoingEmail.new(@email)
       end
 
       it "should open an smtp connection to localhost port 25" do
         Net::SMTP.should_receive(:start).with("localhost", 25)
-        OutgoingEmail.new(@email).send
+        @outgoing.send
       end
 
       it "should send an email to foo@bar.com" do
         smtp = mock
         smtp.should_receive(:send_message).with(anything(), anything(), ["foo@bar.com"]).and_return(mock(message: ""))
         Net::SMTP.should_receive(:start).and_yield(smtp)
-        OutgoingEmail.new(@email).send
+        @outgoing.send
       end
 
       it "should use data_forward to figure out what to send" do
         smtp = mock
-        OutgoingEmail.any_instance.should_receive(:data_to_forward).and_return("My altered data")
+        @outgoing.should_receive(:data_to_forward).and_return("My altered data")
         smtp.should_receive(:send_message).with("My altered data", anything(), anything()).and_return(mock(message: ""))
         Net::SMTP.should_receive(:start).and_yield(smtp)
-        OutgoingEmail.new(@email).send
+        @outgoing.send
       end
 
       it "should use data by default to figure out what to send" do
         smtp = mock
-        @email.should_receive(:data).at_least(:once).and_return("My original data")
         smtp.should_receive(:send_message).with("My original data", anything(), anything()).and_return(mock(message: ""))
         Net::SMTP.should_receive(:start).and_yield(smtp)
-        OutgoingEmail.new(@email).send
-      end
-    end
-
-    it "should send an email to the list of addresses specified in deliveries_to_forward" do
-      email = Email.new
-      delivery_to_forward = mock(:update_attribute => nil)
-      delivery_to_forward.stub_chain(:address, :text).and_return("foo@foo.com")
-      OutgoingEmail.any_instance.should_receive(:deliveries_to_forward).at_least(:once).and_return([delivery_to_forward])    
-      smtp = mock
-      smtp.should_receive(:send_message).with(anything(), anything(), ["foo@foo.com"]).and_return(mock(message: ""))
-      Net::SMTP.stub(:start).and_yield(smtp)
-      OutgoingEmail.new(email).send
-    end
-
-    context "deliveries_to_forward is empty" do
-      let(:email) { Email.create!(to: "foo@bar.com") }
-      before :each do
-        OutgoingEmail.any_instance.stub(:deliveries_to_forward).and_return([])
+        @outgoing.send
       end
 
-      it "should send no emails" do
-        Net::SMTP.should_not_receive(:start)
-        OutgoingEmail.new(email).send
-      end
-    end
-
-    context "an email with one destination" do
-      before :each do
-        @email = Email.create!(to: "foo@bar.com")
-        # Don't want to actually send anything
-        Net::SMTP.stub(:start)
+      it "should send an email to the list of addresses specified in deliveries_to_forward" do
+        delivery_to_forward = mock(:update_attribute => nil)
+        delivery_to_forward.stub_chain(:address, :text).and_return("foo@foo.com")
+        @outgoing.should_receive(:deliveries_to_forward).at_least(:once).and_return([delivery_to_forward])    
+        smtp = mock
+        smtp.should_receive(:send_message).with(anything(), anything(), ["foo@foo.com"]).and_return(mock(message: ""))
+        Net::SMTP.stub(:start).and_yield(smtp)
+        @outgoing.send
       end
 
-      it "should record to which destinations the email has been sent" do
-        @email.deliveries.first.sent?.should be_false
+      context "deliveries_to_forward is empty" do
+        before :each do
+          @outgoing.stub(:deliveries_to_forward).and_return([])
+        end
+
+        it "should send no emails" do
+          Net::SMTP.should_not_receive(:start)
+          @outgoing.send
+        end
       end
 
-      it "should record to which destinations the email has been sent" do
-        OutgoingEmail.new(@email).send
-        @email.deliveries.first.sent?.should be_true
+      context "don't actually send anything" do
+        before :each do
+          Net::SMTP.stub(:start)
+        end
+
+        it "should record to which destinations the email has been sent" do
+          @email.deliveries.first.sent?.should be_false
+        end
+
+        it "should record to which destinations the email has been sent" do
+          @outgoing.send
+          @email.deliveries.first.sent?.should be_true
+        end
       end
     end
   end
