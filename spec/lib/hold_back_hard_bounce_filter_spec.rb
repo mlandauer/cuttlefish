fb8504bb774d11ce2d78a3f302b3aa7ea6a6a10d
Matthew Landauer <matthew@openaustralia.org>
2013-04-16 07:17:00 +1000

    Add tests for the other methods in HoldBackHardBounceFilter

--- spec/lib/hold_back_hard_bounce_filter_spec.rb
+++ spec/lib/hold_back_hard_bounce_filter_spec.rb
@@ -1,21 +1,34 @@
 require "spec_helper"
 
 describe HoldBackHardBounceFilter do
-  let(:delivery) { mock }
+  let(:delivery) { mock(from: "foo@foo.com", to: ["bar@foo.com"], data: "my original data") }
+  let(:filter) { HoldBackHardBounceFilter.new(delivery) }
+
+  describe "#from" do
+    it { filter.from.should == "foo@foo.com" }
+  end
+
+  describe "#to" do
+    it { filter.to.should == ["bar@foo.com"]}
+  end
+
+  describe "#data" do
+    it { filter.data.should == "my original data"}
+  end
 
   describe "#send?" do
     context "an address where an email was succesfully sent before" do
       before :each do
         delivery.stub_chain(:address, :status).and_return("delivered")
       end
-      it { HoldBackHardBounceFilter.new(delivery).send?.should be_true }
+      it { filter.send?.should be_true }
     end
 
     context "an address where an email hard_bounced most recently" do
       before :each do
         delivery.stub_chain(:address, :status).and_return("hard_bounce")
       end
-      it { HoldBackHardBounceFilter.new(delivery).send?.should be_false }
+      it { filter.send?.should be_false }
     end
   end
 
