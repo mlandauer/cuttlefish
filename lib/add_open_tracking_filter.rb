9dfd1dbc048ea99afc356dec456e798e392f88fa
Matthew Landauer <matthew@openaustralia.org>
2013-04-16 09:35:29 +1000

    Use the delivery id in the tracking image

--- lib/add_open_tracking_filter.rb
+++ lib/add_open_tracking_filter.rb
@@ -6,7 +6,7 @@ class AddOpenTrackingFilter < DeliveryFilter
   def data
     mail = Mail.new(delivery.data)
     new_html = mail.parts.first.body.decoded +
-      image_tag(delivery_open_track_url(:id => "123", :host => Rails.configuration.action_mailer.default_url_options[:host], :format => :gif), :alt => nil)
+      image_tag(delivery_open_track_url(:id => original.id, :host => Rails.configuration.action_mailer.default_url_options[:host], :format => :gif), :alt => nil)
     mail.parts.first.body = new_html
     mail.encoded
   end
--- spec/lib/add_open_tracking_filter_spec.rb
+++ spec/lib/add_open_tracking_filter_spec.rb
@@ -11,11 +11,12 @@ describe AddOpenTrackingFilter do
           end
         end
       end
-      let(:filter) { AddOpenTrackingFilter.new(mock(:data => mail.encoded)) }
+      let(:filter) { AddOpenTrackingFilter.new(mock(:data => mail.encoded, :id => "673")) }
 
+      # TODO Use a hash to generate the id in the image so that it can't be guessed
       it "should insert an image at the bottom of the html" do
         Mail.new(filter.data).parts.first.body.should ==
-          '<h1>This is HTML</h1><img src="http://cuttlefish.example.org/o123.gif" />'
+          '<h1>This is HTML</h1><img src="http://cuttlefish.example.org/o673.gif" />'
       end
     end
   end
