559162ca00b32b3a303d728111afa202dcfef680
Matthew Landauer <matthew@openaustralia.org>
2013-04-16 14:35:04 +1000

    Remove done TODO comment

--- app/controllers/delivery_controller.rb
+++ app/controllers/delivery_controller.rb
@@ -2,7 +2,6 @@ class DeliveryController < ApplicationController
   def open_track
     Delivery.find(params[:id]).open_events.create!(user_agent: request.env['HTTP_USER_AGENT'])
     # TODO Check that we are asking for a gif and only accept those for the time being
-    # TODO Record it
     # This sends a 1x1 transparent gif
     send_data(Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="), :type => "image/gif", :disposition => "inline")
   end
