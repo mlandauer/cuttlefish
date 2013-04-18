e031d270f10ca66391e4e8a6e8089613d62033e4
Matthew Landauer <matthew@openaustralia.org>
2013-04-19 08:51:53 +1000

    Pass on all Rails.configuration.action_mailer.default_url_options to url generation for open tracking image

--- lib/add_open_tracking_filter.rb
+++ lib/add_open_tracking_filter.rb
@@ -6,7 +6,7 @@ class AddOpenTrackingFilter < DeliveryFilter
   def data
     if has_html_part?
       delivery.set_open_tracked!
-      append_to_html(image_tag(delivery_open_track_url(:hash => open_tracked_hash, :host => host, :format => :gif), :alt => nil))
+      append_to_html(image_tag(delivery_open_track_url(default_url_options.merge(:hash => open_tracked_hash, :format => :gif)), :alt => nil))
     else
       delivery.data
     end
@@ -28,10 +28,9 @@ class AddOpenTrackingFilter < DeliveryFilter
     !!mail.html_part
   end
 
-  def host
-    options = Rails.configuration.action_mailer.default_url_options
-    if options
-      options[:host]
+  def default_url_options
+    if Rails.configuration.action_mailer.default_url_options
+      Rails.configuration.action_mailer.default_url_options
     else
       raise "Set config.action_mailer.default_url_options in config/environments/#{Rails.env}.rb"
     end
