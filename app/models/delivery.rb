89f15817f0a55755db40032306b9bc96193bd7cb
Matthew Landauer <matthew@openaustralia.org>
2013-04-18 15:36:50 +1000

    Add display of open rate

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -6,6 +6,18 @@ class Delivery < ActiveRecord::Base
 
   after_save :update_status!
   
+  def self.today
+    # Currently created_at on deliveries doesn't get set so have to get the time from emails
+    # TODO Get rid of join
+    joins(:email).where('emails.created_at > ?', Date.today.beginning_of_day)
+  end
+
+  def self.this_week
+    # Currently created_at on deliveries doesn't get set so have to get the time from emails
+    # TODO Get rid of join
+    joins(:email).where('emails.created_at > ?', 7.days.ago)
+  end
+
   # This delivery is being open tracked
   def set_open_tracked!
     # TODO: Move the salt to configuration
