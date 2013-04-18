2a4a3a32bfebc92a9cf127503b3de8a0653f2374
Matthew Landauer <matthew@openaustralia.org>
2013-04-19 08:03:31 +1000

    Add example config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Cuttlefish::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # TODO Extract this into app configuration
+  config.action_mailer.default_url_options = { :host => 'cuttlefish.yourdomain', :protocol => "https" }
+
   # Send our own outgoing email through Cuttlefish
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 2525 }
 end
