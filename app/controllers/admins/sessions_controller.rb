1cbfb62f314f53dce47b31c3eca74fba8dd75408
Matthew Landauer <matthew@openaustralia.org>
2013-04-19 11:44:55 +1000

    When there are no admins force the first user to register

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,2 +1,9 @@
 class Admins::SessionsController < Devise::SessionsController
+  def new
+    if Admin.first.nil?
+      redirect_to new_admin_registration_url
+    else
+      super
+    end
+  end
 end
