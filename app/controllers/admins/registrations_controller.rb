a0a42f204722599082e2a38f971eab7864125eee
Matthew Landauer <matthew@openaustralia.org>
2013-04-19 11:56:30 +1000

    If there are already users then registration won't work

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,3 +1,11 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   layout "login"
+
+  def new
+    if Admin.first.nil?
+      super
+    else
+      redirect_to new_admin_session_url
+    end
+  end
 end
