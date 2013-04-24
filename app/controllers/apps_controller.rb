075e970dee26b41ed918a009fa80e03d4f81383f
Matthew Landauer <matthew@openaustralia.org>
2013-04-24 20:07:23 +1000

    A little tiny bit of error checking on the form

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -9,11 +9,14 @@ class AppsController < ApplicationController
   end
 
   def create
-    @app = App.create(app_parameters)
-    # TODO: You know. error handling
-    flash[:notice] = "App #{@app.name} successfully created"
-    # TODO: Redirect to the app page to show instructions on how to send mail for this app
-    redirect_to apps_path
+    @app = App.new(app_parameters)
+    if @app.save
+      flash[:notice] = "App #{@app.name} successfully created"
+      # TODO: Redirect to the app page to show instructions on how to send mail for this app
+      redirect_to apps_path
+    else
+      render :new
+    end
   end
 
   private
