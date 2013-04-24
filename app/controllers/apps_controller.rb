83b9a2c37446133fe8a780213411a0966564f095
Matthew Landauer <matthew@openaustralia.org>
2013-04-24 18:03:20 +1000

    Can create an app

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -5,5 +5,20 @@ class AppsController < ApplicationController
   def new
     # TODO Extract this
     @default_open_tracking_domain = Rails.configuration.action_mailer.default_url_options[:host]
+    @app = App.new
+  end
+
+  def create
+    @app = App.create(app_parameters)
+    # TODO: You know. error handling
+    flash[:notice] = "App #{@app.name} successfully created"
+    # TODO: Redirect to the app page to show instructions on how to send mail for this app
+    redirect_to apps_path
+  end
+
+  private
+
+  def app_parameters
+    params.require(:app).permit(:name, :description, :url, :open_tracking_domain)
   end
 end
