c9944372b708c9b886b0e6054d8d0ca7a9ede0ea
Matthew Landauer <matthew@openaustralia.org>
2013-04-27 09:44:08 +1000

    Rename controller action

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -1,9 +1,9 @@
 class TrackingController < ApplicationController
-  # We need open_track to be accessible by anyone without authentication
+  # We need this controller to be accessible by anyone without authentication
   skip_filter :authenticate_admin!
   # SSL redirection is also disabled for this controller. See ApplicationController force_ssl.
 
-  def open_track
+  def open
     Delivery.find_by!(open_tracked_hash: params[:hash]).open_events.create!(
       user_agent: request.env['HTTP_USER_AGENT'],
       referer: request.referer,
--- spec/controllers/tracking_controller_spec.rb
+++ spec/controllers/tracking_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe TrackingController do
-  describe "#open_track" do
+  describe "#open" do
     context "A delivery" do
       before :each do
         email = Email.create!
@@ -9,12 +9,12 @@ describe TrackingController do
       end
 
       it "should be succesful when the correct hash is used" do
-        get :open_track, hash: "sdhf"
+        get :open, hash: "sdhf"
         expect(response).to be_success
       end
 
       it "should 404 when hash isn't recognised" do
-        expect { get :open_track, :hash => "123"}.to raise_error(ActiveRecord::RecordNotFound)
+        expect { get :open, :hash => "123"}.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
