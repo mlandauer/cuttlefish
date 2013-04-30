b7c719560a2186ae35e17826bcbe7f969aa312d7
Matthew Landauer <matthew@openaustralia.org>
2013-04-30 14:21:10 +1000

    Stop RegistrationsController#create from accepting requests when an admin already exists

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,11 +1,10 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   layout "login", :except => [:edit, :update]
+  before_filter :check_first_user, :only => [:new, :create]
 
-  def new
-    if Admin.first.nil?
-      super
-    else
-      redirect_to new_admin_session_url
-    end
+  private
+
+  def check_first_user
+    redirect_to new_admin_session_url if Admin.first
   end
 end
---
+++ spec/controllers/admins/registrations_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Admins::RegistrationsController do
+  before :each do
+    request.env["devise.mapping"] = Devise.mappings[:admin]
+    request.env['HTTPS'] = 'on'
+  end
+
+  context "This a new install of Cuttlefish with no users" do
+    it "should allow an admin to register" do
+      get :new
+      response.should be_success
+    end
+
+    it "should allow an admin to register" do
+      post :create
+      response.should be_success
+    end
+  end
+
+  context "There is already one admin registered" do
+    let(:admin) { Admin.create!(:email => "foo@bar.com", :password => "guess this") }
+    before :each do
+      admin
+    end
+
+    it "should not allow an admin to register" do
+      get :new
+      response.should_not be_success
+    end
+
+    it "should not allow an admin to register" do
+      post :create
+      response.should_not be_success
+    end
+
+    it "should allow an admin to update their account details" do
+      sign_in admin
+      get :edit
+      response.should be_success
+    end
+
+    it "should allow an admin to update their account details" do
+      sign_in admin
+      post :update
+      response.should be_success
+    end
+  end
+end
+
