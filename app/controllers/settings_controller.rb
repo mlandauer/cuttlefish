78111c44940c601498258f03ef07b037691f38a9
Matthew Landauer <matthew@openaustralia.org>
2013-05-03 10:30:13 +1000

    Add ui for settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,2 +1,11 @@
 class SettingsController < ApplicationController
+  def edit
+    @smtp_all_authenticated = Settings.smtp_all_authenticated
+  end
+
+  def update
+    Settings.smtp_all_authenticated = !!params[:smtp_all_authenticated]
+    flash[:notice] = "Global settings updated"
+    redirect_to edit_settings_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Cuttlefish::Application.routes.draw do
     end
   end
 
+  resource :settings, :only => [:edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
