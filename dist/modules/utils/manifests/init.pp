50b11afac46e130e128e7d25645184425ec5d0bb
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-06-02 23:48:38 +1000

    Make resource application ordering explicit

--- dist/modules/utils/manifests/init.pp
+++ dist/modules/utils/manifests/init.pp
@@ -1,19 +1,19 @@
 class utils {
 
-  include utils::curl
-  include utils::vim
-  include utils::screen
-  include utils::network
-  include utils::tmux
-  include utils::facterpath
+  class {'utils::curl':} ->
+  class {'utils::vim':} ->
+  class {'utils::screen':}
+  class {'utils::network':} ->
+  class {'utils::tmux':} ->
+  class {'utils::facterpath':} ->
 
   package { [ 'less', 'logtail', 'tofrodos' ]:
     ensure => present
-  }
+  } ->
 
   package { [ 'dbus', 'consolekit' ]:
     ensure => absent
-  }
+  } ->
 
   group { 'admin':
     ensure => present,
