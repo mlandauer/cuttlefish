2b2ca6cdb9856f36d7d1e488dc3de5a9c9724a1b
Matthew Landauer <matthew@openaustralia.org>
2013-06-06 10:02:03 +1000

    Add test for link tracking action in TrackingController

--- spec/controllers/tracking_controller_spec.rb
+++ spec/controllers/tracking_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe TrackingController do
   describe "#open" do
     before :each do
-      delivery = FactoryGirl.create(:delivery, id: 101)
+      FactoryGirl.create(:delivery, id: 101)
     end
 
     it "should be succesful when the correct hash is used" do
@@ -14,6 +14,26 @@ describe TrackingController do
 
     it "should 404 when hash isn't recognised" do
       expect { get :open, delivery_id: 101, hash: "123"}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe "#link" do
+    before :each do
+      FactoryGirl.create(:delivery_link, id: 204)
+      DeliveryLink.any_instance.stub(url: "http://foo.com")
+    end
+
+    it "should redirect when the correct hash and id are used" do
+      get :link, delivery_link_id: 204, hash: "542bae7ec2904c85b945b56072c726d8507fc58a"
+      expect(response).to redirect_to("http://foo.com")
+    end
+
+    it "should 404 when the wrong id is used" do
+      expect { get :link, delivery_link_id: 122, hash: "a5ff8760c7cf5763a4008c338d617f71542e362f"}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "should 4040 when the wrong hash is used" do
+      expect { get :link, delivery_link_id: 204, hash: "123"}.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
