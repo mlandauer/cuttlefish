dbb3459a916ddd286f2bdb564e1d37cb1a97a98d
Matthew Landauer <matthew@openaustralia.org>
2013-06-06 16:13:40 +1000

    Implement logging of link event

--- app/models/delivery_link.rb
+++ app/models/delivery_link.rb
@@ -17,5 +17,10 @@ class DeliveryLink < ActiveRecord::Base
   end
 
   def add_link_event(request)
+    link_events.create!(
+      user_agent: request.env['HTTP_USER_AGENT'],
+      referer: request.referer,
+      ip: request.remote_ip
+    )    
   end
 end
--- spec/models/delivery_link_spec.rb
+++ spec/models/delivery_link_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe DeliveryLink do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#add_link_event" do
+    it "should log the link event with some info from the current request" do
+      request = mock("Request", env: {"HTTP_USER_AGENT" => "some user agent info"},
+        referer: "http://foo.com", remote_ip: "1.2.3.4")
+      delivery_link = FactoryGirl.create(:delivery_link)
+      delivery_link.add_link_event(request)
+      delivery_link.link_events.count.should == 1
+      e = delivery_link.link_events.first
+      e.user_agent.should == "some user agent info"
+      e.referer.should == "http://foo.com"
+      e.ip.should == "1.2.3.4"
+    end
+  end
 end
