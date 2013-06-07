c2298820f87bd7abc36ea98863208640ecd306ad
Matthew Landauer <matthew@openaustralia.org>
2013-06-08 03:39:58 +1000

    Removed unused methods

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,27 +4,6 @@ class Address < ActiveRecord::Base
   has_many :postfix_log_lines, through: :deliveries
   has_many :emails_received, through: :deliveries, source: :email
 
-  # All addresses that have been sent mail
-  def self.all_received_email
-    Address.joins(:emails_received).group(:address_id)
-  end
-
-  # All addresses that have sent mail
-  def self.all_sent_email
-    Address.joins(:emails_sent).group(:from_address_id)
-  end
-
-  # Time this email address was last sent an email (doesn't necessarily mean that it arrived)
-  def time_last_received
-    a = emails_received.order("created_at DESC").first
-    a.created_at if a
-  end
-
-  def time_last_sent
-    a = emails_sent.order("created_at DESC").first
-    a.created_at if a
-  end
-
   def emails
     Email.joins(:from_address, :to_addresses).where("addresses.id = ? OR deliveries.address_id = ?", id, id)
   end
