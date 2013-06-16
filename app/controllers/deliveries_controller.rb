980e311890073b5932a47460f15379ef72a442f1
Matthew Landauer <matthew@openaustralia.org>
2013-06-16 17:16:42 +1000

    Can now filter list of deliveries

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,6 +1,8 @@
 class DeliveriesController < ApplicationController
   def index
-    @deliveries = Delivery.all.includes(:open_events, :link_events, :postfix_log_lines, :email, :address).order("created_at DESC").page(params[:page])
+    @status = params[:status]
+    @deliveries = @status.nil? ? Delivery.all : Delivery.where(status: @status)
+    @deliveries = @deliveries.includes(:open_events, :link_events, :postfix_log_lines, :email, :address).order("created_at DESC").page(params[:page])
   end
 
   def show
