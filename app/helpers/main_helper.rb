6b987d04fde6cd5e3ae320387999f06437b09563
Matthew Landauer <matthew@openaustralia.org>
2013-06-17 10:00:39 +1000

    Don't put links on status counts on addresses pages

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,10 @@
 module MainHelper
+  # Similar to link_to_if but when passed a block behave more like the regular link_to
+  def link_to_if_block(condition, name, options = {}, html_options = {}, &block)
+    if condition
+      link_to(name, options, html_options, &block)
+    else
+      capture(name, &block)
+    end
+  end
 end
