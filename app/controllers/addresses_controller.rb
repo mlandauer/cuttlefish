fb7ad00f18ca839d916a3171e705b1d5434b0d02
Matthew Landauer <matthew@openaustralia.org>
2013-06-18 10:51:51 +1000

    Speed up addresses page by prefetching associations

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,11 @@
 class AddressesController < ApplicationController
   def from
     @address = Address.find(params[:id])
-    @deliveries = @address.deliveries_sent.order("created_at DESC").paginate(page: params[:page])
+    @deliveries = @address.deliveries_sent.includes(:open_events, :link_events, :email => :app).order("deliveries.created_at DESC").paginate(page: params[:page])
   end
 
   def to
     @address = Address.find(params[:id])
-    @deliveries = @address.deliveries_received.order("created_at DESC").paginate(page: params[:page])
+    @deliveries = @address.deliveries_received.includes(:open_events, :link_events, :email => :app).order("created_at DESC").paginate(page: params[:page])
   end
 end
