ce8a06b7ed24387802e025888cee70ab793b5c45
Matthew Landauer <matthew@openaustralia.org>
2013-07-21 13:59:23 +1000

    Show most clicked links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,5 @@
 class LinksController < ApplicationController
+  def index
+    @links = Link.joins(:click_events).group(:link_id).select('links.*, count(link_id) as link_count').order("link_count desc").page(params[:page])
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ActiveRecord::Base
+  has_many :click_events, through: :delivery_links
+  has_many :delivery_links
 end
