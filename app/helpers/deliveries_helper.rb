c00c96a96ce31369a4aac6eb0fa7bde1cd32a428
Matthew Landauer <matthew@oaf.org.au>
2014-07-16 12:48:14 +1000

    Handle status and app being chosen together

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -1,2 +1,9 @@
 module DeliveriesHelper
+  def canonical_deliveries_path(app, status)
+    if app
+      app_deliveries_path(app, status: status)
+    else
+      deliveries_path(status: status)
+    end
+  end
 end
