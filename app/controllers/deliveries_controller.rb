963d2289b02036a35988998da69d0887476441fb
Matthew Landauer <matthew@oaf.org.au>
2014-07-17 15:11:46 +1000

    Make search by email address work

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,9 +1,12 @@
 class DeliveriesController < ApplicationController
   def index
     @status = params[:status]
+    @search = params[:search]
     @app = App.find(params[:app_id]) if params[:app_id]
+
     @deliveries = @status.nil? ? Delivery.all : Delivery.where(status: @status)
     @deliveries = @deliveries.joins(:email).where("emails.app_id" => @app.id) if @app
+    @deliveries = @deliveries.joins(:address).where("addresses.text" => @search) if @search
 
     @deliveries = @deliveries.includes(:open_events, :click_events, :postfix_log_lines, :email, :address).order("deliveries.created_at DESC").page(params[:page])
   end
--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -1,9 +1,9 @@
 module DeliveriesHelper
-  def canonical_deliveries_path(app, status)
+  def canonical_deliveries_path(app, status, search)
     if app
-      app_deliveries_path(app, status: status)
+      app_deliveries_path(app, status: status, search: search)
     else
-      deliveries_path(status: status)
+      deliveries_path(status: status, search: search)
     end
   end
 end
