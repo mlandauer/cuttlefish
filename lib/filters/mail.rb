d2b3a8ba09347566acbc2fb06e471857565e1317
Matthew Landauer <matthew@oaf.org.au>
2014-10-27 13:37:33 +1100

    Multipart email has at least either text or html part

--- lib/filters/mail.rb
+++ lib/filters/mail.rb
@@ -4,24 +4,16 @@ class Filters::Mail < Filters::Delivery
   def data
     mail = Mail.new(delivery.data)
     if mail.multipart?
-      if mail.html_part || mail.text_part
-        mail.html_part.body = process_html(mail.html_part.body.decoded) if mail.html_part
-        mail.text_part.body = process_text(mail.text_part.body.decoded) if mail.text_part
-        mail.encoded
-      else
-        # If we don't need to change either the html or text part don't risk the conversion back and forth between text and Mail representation.
-        # Just pass stuff straight through instead
-        delivery.data
-      end
+      mail.html_part.body = process_html(mail.html_part.body.decoded) if mail.html_part
+      mail.text_part.body = process_text(mail.text_part.body.decoded) if mail.text_part
     else
       if mail.mime_type == "text/html"
         mail.body = process_html(mail.body.decoded)
-        mail.encoded
       else
         mail.body = process_text(mail.body.decoded)
-        mail.encoded
       end
     end
+    mail.encoded
   end
 
   # Override the following four methods in inherited class
