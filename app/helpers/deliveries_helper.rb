84d953672e9e8f32e625a872e8269d3549d9b9d3
Matthew Landauer <matthew@oaf.org.au>
2014-10-30 12:44:55 +1100

    try to clean html emails for display so that they don't interfere with cuttlefish ui

--- app/helpers/deliveries_helper.rb
+++ app/helpers/deliveries_helper.rb
@@ -10,6 +10,10 @@ module DeliveriesHelper
   def clean_html_email_for_display(html)
     # Inline css so that email styling doesn't interfere with the cuttlefish ui
     # and only show the body of the html
-    Nokogiri::HTML(Premailer.new(html, with_html_string: true).to_inline_css).at("body").to_s.html_safe
+    doc = Nokogiri::HTML(Premailer.new(html, with_html_string: true).to_inline_css)
+    doc.search("style").remove
+    body = doc.at("body")
+    body.name = "div"
+    body.to_s.html_safe
   end
 end
