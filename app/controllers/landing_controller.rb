87a608aad6f11c5dfc2a1fc30e5b84085323f714
Matthew Landauer <matthew@oaf.org.au>
2014-11-19 16:26:47 +1100

    Redirect to dash from landing page if already logged in

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,6 @@ class LandingController < ApplicationController
   skip_filter :authenticate_admin!
 
   def index
+    redirect_to dash_path if current_admin
   end
 end
--- spec/controllers/landing_controller_spec.rb
+++ spec/controllers/landing_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe LandingController do
+  before :each do
+    request.env['HTTPS'] = 'on'
+  end
 
+  describe "GET index" do
+    it do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    context "signed in" do
+      before :each do
+        admin = Admin.create!(email: "foo@bar.com", password: "guess this")
+        sign_in admin
+      end
+
+      it do
+        get :index
+        expect(response).to redirect_to "/dash"
+      end
+    end
+  end
 end
