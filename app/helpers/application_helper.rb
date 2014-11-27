8fb849235cdd6668d51a70a3383d4d0fab134aba
Matthew Landauer <matthew@oaf.org.au>
2014-11-27 13:06:13 +1100

    Whitespace changes only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     flash.each do |type, message|
       # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
       next if message.blank?
-      
+
       type = :success if type == :notice
       type = :error   if type == :alert
       next unless ALERT_TYPES.include?(type)
