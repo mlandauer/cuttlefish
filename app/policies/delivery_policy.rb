a5a39df46cb68256547a07eecbbb921b153b88d4
Matthew Landauer <matthew@oaf.org.au>
2014-11-28 18:15:57 +1100

    Revert "Speed up deliveries index again"
    This reverts commit 9719329c7c4e21891c101a9d08ed31a0e86123b8.

--- app/policies/delivery_policy.rb
+++ app/policies/delivery_policy.rb
@@ -1,9 +1,7 @@
 class DeliveryPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      # Avoid using join here as it was a lot slower
-      app_ids = AppPolicy::Scope.new(user, App).resolve.pluck(:id)
-      scope.where(app_id: app_ids)
+      scope.joins(:email => :app).where("apps.team_id" => user.team_id)
     end
   end
 end
