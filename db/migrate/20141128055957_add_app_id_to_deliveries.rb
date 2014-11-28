e1d620864bc33e1e3f31ca6b004bc8e00a6bcafb
Matthew Landauer <matthew@oaf.org.au>
2014-11-28 17:34:50 +1100

    Denormalise and copy app_id to deliveries as well

---
+++ db/migrate/20141128055957_add_app_id_to_deliveries.rb
@@ -0,0 +1,7 @@
+class AddAppIdToDeliveries < ActiveRecord::Migration
+  def change
+    add_column :deliveries, :app_id, :integer
+    add_index :deliveries, :app_id
+    Delivery.connection.execute('UPDATE deliveries JOIN emails ON deliveries.email_id = emails.id SET deliveries.app_id = emails.app_id')
+  end
+end
