ddaf74e26fce63b5557c0b4f458099dadcd8ea8f
Matthew Landauer <matthew@oaf.org.au>
2014-11-30 22:38:04 +1100

    Update app_id in deliveries

---
+++ db/migrate/20141130113657_update_app_id_in_deliveries.rb
@@ -0,0 +1,5 @@
+class UpdateAppIdInDeliveries < ActiveRecord::Migration
+  def change
+    Delivery.connection.execute('UPDATE deliveries JOIN emails ON deliveries.email_id = emails.id SET deliveries.app_id = emails.app_id')
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128055957) do
+ActiveRecord::Schema.define(version: 20141130113657) do
 
   create_table "addresses", force: true do |t|
     t.string   "text"
