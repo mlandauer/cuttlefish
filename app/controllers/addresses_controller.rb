a39dcb1f9abbff26e4e3698a631707909969f51a
Matthew Landauer <matthew@oaf.org.au>
2014-12-01 06:53:38 +1100

    Even guessing email addresses avoid information leak

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,13 @@
 class AddressesController < ApplicationController
   def from
-    @address = Address.friendly.find(params[:id])
+    # Avoid information leak by not revealing whether this email address has been seen before
+    @address = Address.find_or_initialize_by(text: params[:id])
     @deliveries = @address.deliveries_sent(current_admin.team).includes(:open_events, :click_events, :email => :app).order("deliveries.created_at DESC").paginate(page: params[:page])
   end
 
   def to
-    @address = Address.friendly.find(params[:id])
+    # Avoid information leak by not revealing whether this email address has been seen before
+    @address = Address.find_or_initialize_by(text: params[:id])
     @deliveries = @address.deliveries_received(current_admin.team).includes(:open_events, :click_events, :email => :app).order("deliveries.created_at DESC").paginate(page: params[:page])
   end
 end
