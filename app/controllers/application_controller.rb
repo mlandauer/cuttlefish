e0a939b4b7d3075f37728b0a78abf7b2ec8ba279
Matthew Landauer <matthew@oaf.org.au>
2014-12-01 09:42:58 +1100

    Allow admin to pick their name on accepting invitation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:accept_invitation) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
   end
 
