187e22d6a49fd6f96e6ba6b7f3d5e22929cf7d84
Matthew Landauer <matthew@oaf.org.au>
2014-12-01 15:23:01 +1100

    Attach the first user to the first team

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -7,4 +7,10 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   def check_first_user
     redirect_to new_admin_session_url if Admin.first
   end
+
+  def sign_up_params
+    # The first user is attached to the first team
+    team = Team.find_or_create_by(id: 1)
+    devise_parameter_sanitizer.sanitize(:sign_up).merge(team_id: team.id)
+  end
 end
