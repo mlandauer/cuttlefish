203c125604a5ed73e55e369cc5073282a877c818
Matthew Landauer <matthew@oaf.org.au>
2014-12-04 08:36:23 +1100

    Move hash salt secret to environment variable. Fixes #96

--- lib/hash_id.rb
+++ lib/hash_id.rb
@@ -1,8 +1,6 @@
 module HashId
   def self.hash(id)
-    # TODO: Move the salt to configuration
-    salt = "my salt"
-    Digest::SHA1.hexdigest(salt + id.to_s)
+    Digest::SHA1.hexdigest(ENV["CUTTLEFISH_HASH_SALT"] + id.to_s)
   end
 
   def self.valid?(id, h)
