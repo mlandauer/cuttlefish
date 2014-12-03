81f59df6cc387e051c2710f1ca6a4d5931d38ac4
Matthew Landauer <matthew@oaf.org.au>
2014-12-03 15:03:20 +1100

    Whitespace change only

--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module Cuttlefish
     #####################################################
 
     config.cuttlefish_domain = ENV["CUTTLEFISH_DOMAIN"] || "cuttlefish.io"
-    
+
     config.postfix_smtp_host = ENV["POSTFIX_SMTP_HOST"] || "localhost"
     config.postfix_smtp_port = ENV["POSTFIX_SMTP_PORT"] ? ENV["POSTFIX_SMTP_PORT"].to_i : 25
 
