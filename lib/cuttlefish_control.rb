0a7b4adf00f3b9d0641284b9c2b8c6d280cd5c01
Matthew Landauer <matthew@oaf.org.au>
2014-12-04 09:54:35 +1100

    Extract log daemon into its own class

--- lib/cuttlefish_control.rb
+++ lib/cuttlefish_control.rb
@@ -35,34 +35,6 @@ module CuttlefishControl
 
     file = "/var/log/mail/mail.log"
     puts "Sucking up log entries in #{file}..."
-    begin
-      while true
-        if File.exists?(file)
-          File::Tail::Logfile.open(file) do |log|
-            log.tail do |line|
-              PostfixLogLine.transaction do
-                log_line = PostfixLogLine.create_from_line(line)
-                # Check if an email needs to be blacklisted
-                # TODO Move this domain logic somewhere sensible
-                if log_line && log_line.status == "hard_bounce"
-                  # We don't want to save duplicates
-                  if BlackList.find_by(team_id: log_line.delivery.app.team_id, address: log_line.delivery.address).nil?
-                    BlackList.create(team_id: log_line.delivery.app.team_id, address: log_line.delivery.address, caused_by_delivery: log_line.delivery)
-                  end
-                end
-              end
-            end
-          end
-        else
-          sleep(10)
-        end
-      end
-    rescue SignalException => e
-      if e.to_s == "SIGTERM"
-        puts "Received SIGTERM. Shutting down..."
-      else
-        raise e
-      end
-    end
+    CuttlefishLogDaemon.start(file)
   end
 end
