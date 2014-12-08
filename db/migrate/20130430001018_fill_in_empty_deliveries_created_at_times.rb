e77265d0b6d3b17436213134dc91dce5209b8abe
Matthew Landauer <matthew@oaf.org.au>
2014-12-09 07:44:57 +1100

    Don't need to migrate old data anymore

--- db/migrate/20130430001018_fill_in_empty_deliveries_created_at_times.rb
+++ db/migrate/20130430001018_fill_in_empty_deliveries_created_at_times.rb
@@ -1,6 +1,6 @@
 class FillInEmptyDeliveriesCreatedAtTimes < ActiveRecord::Migration
   def change
-    Delivery.where("deliveries.created_at IS NULL").joins(:email).update_all("deliveries.created_at = emails.created_at")
-    Delivery.where("deliveries.updated_at IS NULL").joins(:email).update_all("deliveries.updated_at = emails.updated_at")
+    # Delivery.where("deliveries.created_at IS NULL").joins(:email).update_all("deliveries.created_at = emails.created_at")
+    # Delivery.where("deliveries.updated_at IS NULL").joins(:email).update_all("deliveries.updated_at = emails.updated_at")
   end
 end
