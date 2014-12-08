bf5e0675f07d512b0fdb9faf01f9ae05c4761789
Matthew Landauer <matthew@oaf.org.au>
2014-12-09 07:41:26 +1100

    Don't need to migrate old data anymore

--- db/migrate/20130414051352_add_postfix_queue_id_to_deliveries.rb
+++ db/migrate/20130414051352_add_postfix_queue_id_to_deliveries.rb
@@ -2,7 +2,7 @@ class AddPostfixQueueIdToDeliveries < ActiveRecord::Migration
   def change
     add_column :deliveries, :postfix_queue_id, :string
     add_index :deliveries, :postfix_queue_id
-    Delivery.where(sent: true).joins(:email).update_all("deliveries.postfix_queue_id = emails.postfix_queue_id")
+    #Delivery.where(sent: true).joins(:email).update_all("deliveries.postfix_queue_id = emails.postfix_queue_id")
     remove_column :emails, :postfix_queue_id
   end
 end
