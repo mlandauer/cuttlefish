b3d58ae6c20c14305ecbe91ee8f295f44a0c6dff
Matthew Landauer <matthew@oaf.org.au>
2014-12-12 10:47:53 +1100

    Allow devise emails from address to be overridden in the environment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "contact@oaf.org.au"
+  config.mailer_sender = Rails.configuration.devise_emails_from
 
   # Configure the class responsible to send e-mails.
   config.mailer = "InternalMailer"
