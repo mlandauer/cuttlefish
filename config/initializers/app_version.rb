ea25aaddebe32d20a27610afd6393be632242d92
Matthew Landauer <matthew@oaf.org.au>
2014-12-14 17:59:30 +1100

    Only use the first 7 digits of the sha for the version

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,3 +1,3 @@
 unless defined?(APP_VERSION)
-  APP_VERSION = Rails.env.production? ? File.read(File.join(Rails.root, "REVISION")) : `git describe --always`
+  APP_VERSION = Rails.env.production? ? File.read(File.join(Rails.root, "REVISION"))[0..6] : `git describe --always`
 end
