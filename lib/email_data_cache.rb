c1314bd41097342d8e787cbdcd066765d9f6c9ce
Matthew Landauer <matthew@oaf.org.au>
2014-12-15 19:07:00 +1100

    Inline methods

--- lib/email_data_cache.rb
+++ lib/email_data_cache.rb
@@ -6,39 +6,25 @@ class EmailDataCache
   end
 
   def set(id, data)
-    EmailDataCache.set(scope, id, data)
+    save_data_to_filesystem(id, data)
+    cleanup_filesystem_data_store
   end
 
   def get(id)
-    EmailDataCache.get(scope, id)
+    File.read(data_filesystem_path(id)) if is_data_on_filesystem?(id)
   end
 
   def data_filesystem_directory
-    EmailDataCache.data_filesystem_directory(scope)
+    File.join("db", "emails", scope)
   end
 
   def create_data_filesystem_directory
-    EmailDataCache.create_data_filesystem_directory(scope)
-  end
-  
-  #####
-
-  def self.set(scope, id, data)
-    save_data_to_filesystem(scope, id, data)
-    cleanup_filesystem_data_store(scope)
-  end
-
-  def self.get(scope, id)
-    File.read(data_filesystem_path(scope, id)) if is_data_on_filesystem?(scope, id)
+    FileUtils::mkdir_p(data_filesystem_directory)
   end
 
   def self.max_no_emails_to_store_data
     # By default keep the full content of the last 1000 emails
     1000
-  end
-
-  def self.data_filesystem_directory(scope)
-    File.join("db", "emails", scope)
   end
 
   # Won't throw an exception when filename doesn't exist
@@ -50,39 +36,35 @@ class EmailDataCache
     end
   end
 
-  def self.create_data_filesystem_directory(scope)
-    FileUtils::mkdir_p(data_filesystem_directory(scope))
-  end
-
   private
 
-  def self.data_filesystem_path(scope, id)
-    File.join(data_filesystem_directory(scope), "#{id}.eml")
+  def data_filesystem_path(id)
+    File.join(data_filesystem_directory, "#{id}.eml")
   end
 
-  def self.is_data_on_filesystem?(scope, id)
-    File.exists?(data_filesystem_path(scope, id))
+  def is_data_on_filesystem?(id)
+    File.exists?(data_filesystem_path(id))
   end
 
-  def self.save_data_to_filesystem(scope, id, data)
+  def save_data_to_filesystem(id, data)
     # Don't overwrite the data that's already on the filesystem
-    unless is_data_on_filesystem?(scope, id)
+    unless is_data_on_filesystem?(id)
       # Save the data part of the email to the filesystem
-      create_data_filesystem_directory(scope)
-      File.open(data_filesystem_path(scope, id), "w") do |f|
+      create_data_filesystem_directory
+      File.open(data_filesystem_path(id), "w") do |f|
         f.write(data)
       end
     end
   end
 
-  def self.cleanup_filesystem_data_store(scope)
+  def cleanup_filesystem_data_store
     # If there are more than a certain number of stored emails on the filesystem
     # remove the oldest ones
-    entries = Dir.glob(File.join(data_filesystem_directory(scope), "*"))
-    no_to_remove = entries.count - max_no_emails_to_store_data
+    entries = Dir.glob(File.join(data_filesystem_directory, "*"))
+    no_to_remove = entries.count - EmailDataCache.max_no_emails_to_store_data
     if no_to_remove > 0
       # Oldest first
-      entries.sort_by {|f| File.mtime f}[0...no_to_remove].each {|f| safe_file_delete f}
+      entries.sort_by {|f| File.mtime f}[0...no_to_remove].each {|f| EmailDataCache.safe_file_delete f}
     end
   end
 end
