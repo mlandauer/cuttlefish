2a7dc8233ddc2a974190277d8a936da96a165445
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 15:34:14 +1100

    Inherit from a different base class

--- lib/filters/inline_css.rb
+++ lib/filters/inline_css.rb
@@ -1,4 +1,4 @@
-class Filters::InlineCss < Filters::Tracking
+class Filters::InlineCss < Filters::Mail
   def process_html(input, delivery)
     premailer = Premailer.new(input, with_html_string: true, input_encoding: input.encoding.to_s)
     premailer.to_inline_css
