58945e8aacd26bf7bae4f13177482eec4224d6d9
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 14:17:30 +1100

    Extract method

--- lib/filters/mailer_header.rb
+++ lib/filters/mailer_header.rb
@@ -1,6 +1,10 @@
 class Filters::MailerHeader < Filters::Delivery
   def data(delivery)
-    mail = Mail.new(input_data(delivery))
+    data2(delivery.data)
+  end
+
+  def data2(content)
+    mail = Mail.new(input_data2(content))
     mail.header['X-Mailer'] = "Cuttlefish #{APP_VERSION}"
     mail.to_s
   end
