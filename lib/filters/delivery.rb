3b305b9542842eed8d8e1268eca7dfef81cddaf9
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 15:20:57 +1100

    Filters are now much simpler

--- lib/filters/delivery.rb
+++ lib/filters/delivery.rb
@@ -1,16 +1,6 @@
 class Filters::Delivery
-  attr_accessor :previous_filter
-
   def initialize(delivery)
     @delivery = delivery
-  end
-
-  def output_data(content)
-    if previous_filter.nil?
-      filter(content)
-    else
-      filter(previous_filter.output_data(content))
-    end
   end
 
   # Override this method
--- lib/filters/master.rb
+++ lib/filters/master.rb
@@ -1,15 +1,10 @@
 class Filters::Master
   def data(delivery)
-    filter1 = Filters::AddOpenTracking.new(delivery)
-    filter2 = Filters::ClickTracking.new(delivery)
-    filter2.previous_filter = filter1
-    filter3 = Filters::InlineCss.new(delivery)
-    filter3.previous_filter = filter2
-    filter4 = Filters::MailerHeader.new(delivery)
-    filter4.previous_filter = filter3
+    filtered1 = Filters::AddOpenTracking.new(delivery).filter(delivery.data)
+    filtered2 = Filters::ClickTracking.new(delivery).filter(filtered1)
+    filtered3 = Filters::InlineCss.new(delivery).filter(filtered2)
+    filtered4 = Filters::MailerHeader.new(delivery).filter(filtered3)
     # DKIM filter needs to always be the last one
-    filter5 = Filters::Dkim.new(delivery)
-    filter5.previous_filter = filter4
-    filter5.output_data(delivery.data)
+    Filters::Dkim.new(delivery).filter(filtered4)
   end
 end
