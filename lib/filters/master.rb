614e7582961fd85e175476955fead463b91c2758
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 11:49:09 +1100

    Make the filter chain more explicit

--- lib/filters/master.rb
+++ lib/filters/master.rb
@@ -1,7 +1,11 @@
 class Filters::Master
   def data(delivery)
+    filter1 = Filters::AddOpenTracking.new
+    filter2 = Filters::ClickTracking.new(filter1)
+    filter3 = Filters::InlineCss.new(filter2)
+    filter4 = Filters::MailerHeader.new(filter3)
     # DKIM filter needs to always be the last one
-    filtered = Filters::Dkim.new(Filters::MailerHeader.new(Filters::InlineCss.new(Filters::ClickTracking.new(Filters::AddOpenTracking.new))))
-    filtered.data(delivery)
+    filter5 = Filters::Dkim.new(filter4)
+    filter5.data(delivery)
   end
 end
