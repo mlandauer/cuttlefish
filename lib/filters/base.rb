b0432e0854e697497b79183109c97837bd0e88cc
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 16:14:21 +1100

    Extract method

--- lib/filters/base.rb
+++ lib/filters/base.rb
@@ -1,12 +1,16 @@
 class Filters::Base
   attr_reader :delivery
-  
+
   def initialize(delivery)
     @delivery = delivery
   end
 
   # Override this method
   def filter(content)
-    content
+    filter_mail(Mail.new(content)).to_s
+  end
+
+  def filter_mail(mail)
+    mail
   end
 end
--- spec/lib/filters/base_spec.rb
+++ spec/lib/filters/base_spec.rb
@@ -1,10 +1,15 @@
 require "spec_helper"
 
 describe Filters::Base do
-  let(:delivery) { double(from: "foo@foo.com", to: ["bar@foo.com"], data: "my original data") }
+  let(:mail) {
+    Mail.new do
+      body "Some content"
+    end
+  }
+  let(:delivery) { double }
   let(:filter) { Filters::Base.new(delivery) }
 
-  describe "#data" do
-    it { filter.filter(delivery.data).should == "my original data"}
+  describe "#filter" do
+    it { filter.filter(mail.to_s).should == mail.to_s}
   end
 end
