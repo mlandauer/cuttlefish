a2846c475121416ab65abdcd128d59d95b970842
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 16:07:24 +1100

    Extract method

--- lib/filters/master.rb
+++ lib/filters/master.rb
@@ -1,10 +1,15 @@
 class Filters::Master < Filters::Base
   def filter(content)
+    filter_mail(Mail.new(content)).to_s
+  end
+
+  def filter_mail(mail)
+    content = mail.to_s
     filtered1 = Filters::AddOpenTracking.new(delivery).filter(content)
     filtered2 = Filters::ClickTracking.new(delivery).filter(filtered1)
     filtered3 = Filters::InlineCss.new(delivery).filter(filtered2)
     filtered4 = Filters::MailerHeader.new(delivery).filter(filtered3)
     # DKIM filter needs to always be the last one
-    Filters::Dkim.new(delivery).filter(filtered4)
+    Mail.new(Filters::Dkim.new(delivery).filter(filtered4))
   end
 end
