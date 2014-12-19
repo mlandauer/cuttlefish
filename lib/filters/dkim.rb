d8b82281d5bac6e8b157b3a938faa5b9f29e1c11
Matthew Landauer <matthew@oaf.org.au>
2014-12-19 14:14:02 +1100

    Extract method

--- lib/filters/dkim.rb
+++ lib/filters/dkim.rb
@@ -1,13 +1,17 @@
 class Filters::Dkim < Filters::Delivery
   def data(delivery)
-    if active?(delivery)
-      Dkim.sign(input_data(delivery), selector: 'cuttlefish', private_key: delivery.app.dkim_key, domain: delivery.app.from_domain)
+    data2(delivery.data)
+  end
+
+  def data2(content)
+    if active?
+      Dkim.sign(input_data2(content), selector: 'cuttlefish', private_key: @delivery.app.dkim_key, domain: @delivery.app.from_domain)
     else
-      input_data(delivery)
+      input_data2(content)
     end
   end
 
-  def active?(delivery)
-    delivery.app.dkim_enabled && delivery.from_domain == delivery.app.from_domain
+  def active?
+    @delivery.app.dkim_enabled && @delivery.from_domain == @delivery.app.from_domain
   end
 end
