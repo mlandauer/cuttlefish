3855f41344da6fae5187b2130318f79700d4fe08
Matthew Landauer <matthew@oaf.org.au>
2014-12-20 18:50:17 +1100

    Extract into options

--- lib/filters/dkim.rb
+++ lib/filters/dkim.rb
@@ -1,9 +1,22 @@
 class Filters::Dkim < Filters::Base
+  attr_reader :options
+
+  # options: enabled, domain, key, sender_email
+  def initialize(delivery)
+    @delivery = delivery
+    @options = {
+      enabled: delivery.app.dkim_enabled,
+      domain: delivery.app.from_domain,
+      key: delivery.app.dkim_key,
+      sender_email: Rails.configuration.cuttlefish_sender_email
+    }
+  end
+
   def filter_mail(mail)
     if active?(mail)
-      Mail.new(Dkim.sign(mail.to_s, selector: 'cuttlefish', private_key: delivery.app.dkim_key, domain: delivery.app.from_domain))
+      Mail.new(Dkim.sign(mail.to_s, selector: 'cuttlefish', private_key: options[:key], domain: options[:domain]))
     else
-      mail.sender = Rails.configuration.cuttlefish_sender_email
+      mail.sender = options[:sender_email]
       # TODO Sign with DKIM for cuttlefish_sender_email domain if available
       mail
     end
@@ -12,6 +25,6 @@ class Filters::Dkim < Filters::Base
   def active?(mail)
     address = mail.sender || mail.from.first
     from_domain = address.split("@")[1]
-    delivery.app.dkim_enabled && from_domain == delivery.app.from_domain
+    options[:enabled] && from_domain == options[:domain]
   end
 end
