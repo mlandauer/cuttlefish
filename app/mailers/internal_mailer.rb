e7cc558657689e1ec5dd97a2536b2f1a40111399
Matthew Landauer <matthew@oaf.org.au>
2014-12-26 13:44:23 +1100

    Try to fix travis build in slightly hacky way

--- app/mailers/internal_mailer.rb
+++ app/mailers/internal_mailer.rb
@@ -1,14 +1,18 @@
 class InternalMailer < Devise::Mailer
-  default delivery_method_options: {
-    address: Rails.configuration.cuttlefish_domain,
-    port: Rails.configuration.cuttlefish_smtp_port,
-    user_name: App.cuttlefish.smtp_username,
-    password: App.cuttlefish.smtp_password,
-    # So that we don't get a certificate name and host mismatch we're just
-    # disabling the check.
-    openssl_verify_mode: "none",
-    authentication: :plain
-  }
+  # Hacky way to not try to create cuttlefish app if apps table doesn't yet exist.
+  # This can happen when you're doing a "rake db:setup"
+  if ActiveRecord::Base.connection.table_exists? 'apps'
+    default delivery_method_options: {
+      address: Rails.configuration.cuttlefish_domain,
+      port: Rails.configuration.cuttlefish_smtp_port,
+      user_name: App.cuttlefish.smtp_username,
+      password: App.cuttlefish.smtp_password,
+      # So that we don't get a certificate name and host mismatch we're just
+      # disabling the check.
+      openssl_verify_mode: "none",
+      authentication: :plain
+    }
+  end
 
   def invitation_instructions(record, token, opts={})
     opts[:subject] = "#{record.invited_by.display_name} invites you to Cuttlefish"
