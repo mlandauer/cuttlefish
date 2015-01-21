6c2cd74a2e094642827ca3715cd6474a8d361645
Matthew Landauer <matthew@oaf.org.au>
2015-01-21 13:44:05 +1100

    Show user agent family and os family for open events

--- app/models/open_event.rb
+++ app/models/open_event.rb
@@ -1,38 +1,22 @@
 class OpenEvent < ActiveRecord::Base
   belongs_to :delivery, counter_cache: true
 
-  # def ua_family
-  #   user_agent_info["ua_family"]
-  # end
+  def ua_family
+    parsed_user_agent.family
+  end
 
-  # def ua_url
-  #   user_agent_info["ua_url"]
-  # end
-
-  # def os_family
-  #   user_agent_info["os_family"]
-  # end
-
-  # def os_url
-  #   user_agent_info["os_url"]
-  # end
+  def os_family
+    parsed_user_agent.os.family
+  end
 
   private
 
-  # def user_agents_cache_directory
-  #   "db/user_agents"
-  # end
+  def parsed_user_agent
+    @parsed_user_agent ||= user_agent_parser.parse(user_agent)
+  end
 
-  # def user_agent_info_without_caching
-  #   # This is really inefficient. It's going to reload the cache on
-  #   # every single web request
-  #   # TODO Make this efficient
-  #   FileUtils::mkdir_p(user_agents_cache_directory)
-  #   uas_parser = UASparser.new(user_agents_cache_directory)
-  #   uas_parser.parse(user_agent)
-  # end
-
-  # def user_agent_info
-  #   @user_agent_info ||= user_agent_info_without_caching
-  # end
+  # Cache this between requests so that we don't keep reloading the user agent database
+  def user_agent_parser
+    @@user_agent_parser ||= UserAgentParser::Parser.new
+  end
 end
