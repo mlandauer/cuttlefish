d4beccf0f09115610c114398fd763daff1086999
Matthew Landauer <matthew@oaf.org.au>
2015-01-23 12:03:24 +1100

    Add blank clients index page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,4 @@
 class ClientsController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :clients, only: :index
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
