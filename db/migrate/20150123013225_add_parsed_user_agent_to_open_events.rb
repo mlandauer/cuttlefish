f9b894f49ac53ca1c0ae95f65eae712258d4f6d2
Matthew Landauer <matthew@oaf.org.au>
2015-01-23 13:38:56 +1100

    Explicitly inline to try to narrow down what's going on

--- db/migrate/20150123013225_add_parsed_user_agent_to_open_events.rb
+++ db/migrate/20150123013225_add_parsed_user_agent_to_open_events.rb
@@ -5,15 +5,23 @@ class AddParsedUserAgentToOpenEvents < ActiveRecord::Migration
     add_column :open_events, :os_family, :string
     add_column :open_events, :os_version, :string
 
+    parser = UserAgentParser::Parser.new(patterns_path: "lib/regexes.yaml")
     OpenEvent.reset_column_information
     # Resaving open events so that the user agent parsing happens
     count = 0
     OpenEvent.find_each do |open_event|
+      parsed_user_agent = parser.parse(open_event.user_agent)
+
+      calculate_ua_family = parsed_user_agent.family
+      calculate_ua_version = parsed_user_agent.version.to_s if parsed_user_agent.version
+      calculate_os_family = parsed_user_agent.os.family
+      calculate_os_version = parsed_user_agent.os.version.to_s if parsed_user_agent.os.version
+
       open_event.update_attributes(
-        ua_family: open_event.calculate_ua_family,
-        ua_version: open_event.calculate_ua_version,
-        os_family: open_event.calculate_os_family,
-        os_version: open_event.calculate_os_version
+        ua_family: calculate_ua_family,
+        ua_version: calculate_ua_version,
+        os_family: calculate_os_family,
+        os_version: calculate_os_version
       )
 
       count += 1
