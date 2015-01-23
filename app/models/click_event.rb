53ef097d314520d3ec5016698b9b73aa9aed647b
Matthew Landauer <matthew@oaf.org.au>
2015-01-23 12:30:41 +1100

    Use renamed method

--- app/models/click_event.rb
+++ app/models/click_event.rb
@@ -3,11 +3,6 @@ class ClickEvent < ActiveRecord::Base
   delegate :link, to: :delivery_link
   include UserAgent
 
-  alias_method :ua_family, :calculate_ua_family
-  alias_method :ua_version, :calculate_ua_version
-  alias_method :os_family, :calculate_os_family
-  alias_method :os_version, :calculate_os_version
-
   def url
     link.url
   end
--- app/models/open_event.rb
+++ app/models/open_event.rb
@@ -1,9 +1,4 @@
 class OpenEvent < ActiveRecord::Base
   belongs_to :delivery, counter_cache: true
   include UserAgent
-
-  alias_method :ua_family, :calculate_ua_family
-  alias_method :ua_version, :calculate_ua_version
-  alias_method :os_family, :calculate_os_family
-  alias_method :os_version, :calculate_os_version
 end
