df19d9503dc61d94ccb89562de39c709f9e64bc8
Matthew Landauer <matthew@oaf.org.au>
2015-01-30 16:41:50 +1100

    Now actually send email

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -6,4 +6,11 @@ class LandingController < ApplicationController
     @signup_form = SignupForm.new if @poplus
     redirect_to dash_path if current_admin
   end
+
+  def request_invitation
+    @signup_form = SignupForm.new(params[:signup_form])
+    @signup_form.deliver
+    flash[:notice] = "Thanks! You should hear back from us very soon."
+    redirect_to root_path
+  end
 end
--- app/models/signup_form.rb
+++ app/models/signup_form.rb
@@ -9,7 +9,19 @@ class SignupForm < MailForm::Base
     {
       subject: "Access request for cuttlefish.oaf.org.au",
       to: "contact@oaf.org.au",
-      from: %("#{name}" <#{email}>)
+      from: %("#{name}" <#{email}>),
+      # This configuration is shared with devise.
+      # TODO Can't we just make this the default configuration for sending all mail?
+      delivery_method_options: {
+        address: Rails.configuration.cuttlefish_domain,
+        port: Rails.configuration.cuttlefish_smtp_port,
+        user_name: App.cuttlefish.smtp_username,
+        password: App.cuttlefish.smtp_password,
+        # So that we don't get a certificate name and host mismatch we're just
+        # disabling the check.
+        openssl_verify_mode: "none",
+        authentication: :plain
+      }
     }
   end
 end
