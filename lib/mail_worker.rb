31a22751b7e213a061cc19f7c5d9c0b878f65153
Matthew Landauer <matthew@oaf.org.au>
2015-02-17 10:56:45 +1100

    Extract method

--- lib/mail_worker.rb
+++ lib/mail_worker.rb
@@ -3,7 +3,10 @@ class MailWorker
   include Sidekiq::Worker
   def perform(sender, recipients, data, received, completed_at, app_id)
     # TODO Remove sender, received, completed_at because they are not used
-    
+    perform2(recipients, data, app_id)
+  end
+
+  def perform2(recipients, data, app_id)
     ActiveRecord::Base.transaction do
       # Discard the "return path" in sender
       # Take the from instead from the contents of the mail
