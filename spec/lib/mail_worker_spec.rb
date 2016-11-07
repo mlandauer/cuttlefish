22ad0ea5fb39d063cb4b500d20061090270c68c0
Henare Degan <henare.degan@gmail.com>
2016-11-07 18:39:10 +1100

    Fix test data so the test is testing the correct thing
    This was noticed thanks to fb28972. Before this change the error being
    raised was due to the incorrect test data, not because of the exception
    raised by OutgoingDelivery so we weren't getting the error at the point
    we expected.

--- spec/lib/mail_worker_spec.rb
+++ spec/lib/mail_worker_spec.rb
@@ -31,7 +31,7 @@ describe MailWorker, '#perform' do
     allow_any_instance_of(OutgoingDelivery).to receive(:send).and_raise("I can't contact the mail server")
 
     expect {
-      MailWorker.new.perform(["<foo@bar.com>"], "message", app.id)
+      MailWorker.new.perform(["<foo@bar.com>"], Base64.encode64(mail.encoded), app.id)
     }.to raise_error("I can't contact the mail server")
 
     expect(Email.count).to eq 0
