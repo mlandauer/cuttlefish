a322a70eb542c8eefbe9b0fcb744188bf7c764b0
Luke Bacon <luke@equivalentideas.com>
2017-04-05 11:08:50 +1000

    Extract long conditional logic to method

--- app/models/black_list.rb
+++ app/models/black_list.rb
@@ -1,4 +1,8 @@
 class BlackList < ActiveRecord::Base
   belongs_to :address
   belongs_to :caused_by_delivery, class_name: "Delivery"
+
+  def has_record_of_cause?
+    caused_by_delivery.present? && caused_by_delivery.subject.present?
+  end
 end
