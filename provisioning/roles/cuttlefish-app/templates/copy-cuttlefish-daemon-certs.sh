46ea18ad03cc349bc096d4f3bde5fc8be57f47db
Matthew Landauer <mlandauer@gmail.com>
2018-06-25 16:27:38 +1000

    After updating certs copy them to the correct place and let cuttlefish know where they are

---
+++ provisioning/roles/cuttlefish-app/templates/copy-cuttlefish-daemon-certs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# Make sure the certificate and private key files are
+# never world readable, even just for an instant while
+# we're copying them
+umask 077
+
+cp "$RENEWED_LINEAGE/fullchain.pem" /srv/www/shared/
+cp "$RENEWED_LINEAGE/privkey.pem" /srv/www/shared/
+
+# Apply the proper file ownership and permissions for
+# the daemon to read its certificate and key.
+chown deploy /srv/www/shared/fullchain.pem /srv/www/shared/privkey.pem
+chmod 400 /srv/www/shared/fullchain.pem /srv/www/shared/privkey.pem
+
+service cuttlefish-smtp restart >/dev/null
