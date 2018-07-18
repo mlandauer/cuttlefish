235d044770f78033eed2f98a6a3030c823530533
Matthew Landauer <mlandauer@gmail.com>
2018-07-19 06:11:55 +1000

    Remove example field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,11 +18,4 @@ class Types::QueryType < Types::BaseObject
   def emails
     Delivery.all
   end
-
-  # TODO: remove me
-  field :test_field, String, null: false,
-    description: "An example field added by the generator"
-  def test_field
-    "Hello World!"
-  end
 end
