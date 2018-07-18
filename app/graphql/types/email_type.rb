82130323f74c37e4aae8ec4adc66145339571bf3
Matthew Landauer <mlandauer@gmail.com>
2018-07-19 08:46:49 +1000

    Add temporary comments for fields that we should include in the email api

--- app/graphql/types/email_type.rb
+++ app/graphql/types/email_type.rb
@@ -19,3 +19,54 @@ class Types::EmailType < Types::BaseObject
     object.clicked?
   end
 end
+
+# TODO: Fields that need to be included based on what's shown in the
+# admin interface for the delivery#show action
+
+# Delivery:
+# subject
+# status
+# opened?
+# clicked?
+# id
+# app
+# from
+# to
+# created_at
+# postfix_log_lines
+# open_events
+# click_events
+# # TODO: Group these together
+# content_available?
+# html_part
+# text_part
+# data
+#
+# App:
+# id
+# name
+#
+# PostfixLogLine:
+# time
+# dsn
+# extended_status
+#
+# OpenEvent:
+# created_at
+# ua_family
+# ua_version
+# os_family
+# os_version
+# ip
+#
+# ClickEvent:
+# created_at
+# url
+# calculate_ua_family
+# calculate_ua_version
+# calculate_os_family
+# calculate_os_version
+# ip
+#
+# Configuration:
+# max_no_emails_to_store
