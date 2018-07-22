eb292335246e410bc5eecca08feed84fb5a0cc61
Matthew Landauer <mlandauer@gmail.com>
2018-07-23 08:59:57 +1000

    Seperate search in controller to make difference more explicit

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -3,17 +3,20 @@ class DeliveriesController < ApplicationController
   after_action :verify_policy_scoped, only: :index
 
   def index
-    @status = params[:status]
-    @search = params[:search]
     @deliveries = policy_scope(Delivery)
-    if params[:app_id]
-      @app = App.find(params[:app_id])
-      @deliveries = @deliveries.where(app_id: @app.id)
-      @deliveries = @deliveries.joins(:email).where("emails.app_id" => @app.id)
-    end
 
-    @deliveries = @deliveries.where(status: @status) if @status
-    @deliveries = @deliveries.joins(:address).where("addresses.text" => @search) if @search
+    if params[:search]
+      @search = params[:search]
+      @deliveries = @deliveries.joins(:address).where("addresses.text" => @search)
+    else
+      @status = params[:status]
+      if params[:app_id]
+        @app = App.find(params[:app_id])
+        @deliveries = @deliveries.where(app_id: @app.id)
+        @deliveries = @deliveries.joins(:email).where("emails.app_id" => @app.id)
+      end
+      @deliveries = @deliveries.where(status: @status) if @status
+    end
 
     @deliveries = @deliveries.includes(:delivery_links, :postfix_log_lines, :email, :address).order("deliveries.created_at DESC").page(params[:page])
   end
