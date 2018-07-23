fe8f39f747f09f8b36ea1ad9115231db1094d268
Matthew Landauer <mlandauer@gmail.com>
2018-07-24 04:43:52 +1000

    Extract module for api

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -23,52 +23,10 @@ class DeliveriesController < ApplicationController
     @deliveries = @deliveries.includes(:delivery_links, :postfix_log_lines, :email, :address).order("deliveries.created_at DESC").page(params[:page])
   end
 
-  HTTP = GraphQL::Client::HTTP.new("http://localhost:5400/graphql") do
-    def headers(context)
-      # Optionally set any HTTP headers
-      { "Authorization": context[:api_key] }
-    end
-  end
-  SCHEMA = GraphQL::Client.load_schema(HTTP)
-  CLIENT = GraphQL::Client.new(schema: SCHEMA, execute: HTTP)
-  EMAIL_QUERY = CLIENT.parse <<-'GRAPHQL'
-    query($id: ID!) {
-      email(id: $id) {
-        from
-        to
-        subject
-        content {
-          html
-          text
-          source
-        }
-        createdAt
-        status
-        app {
-          id
-          name
-        }
-        opened
-        clicked
-        openEvents {
-          ip
-          userAgent {
-            family
-            version
-          }
-          os {
-            family
-            version
-          }
-          createdAt
-        }
-      }
-    }
-  GRAPHQL
 
   def show
-    result = CLIENT.query(
-      EMAIL_QUERY,
+    result = Cuttlefish::ApiClient::CLIENT.query(
+      Cuttlefish::ApiClient::EMAIL_QUERY,
       variables: {id: params[:id]},
       context: { api_key: current_admin.api_key }
     )
---
+++ lib/cuttlefish/api_client.rb
@@ -0,0 +1,46 @@
+module Cuttlefish::ApiClient
+  HTTP = GraphQL::Client::HTTP.new("http://localhost:5400/graphql") do
+    def headers(context)
+      { "Authorization": context[:api_key] }
+    end
+  end
+
+  SCHEMA = GraphQL::Client.load_schema(HTTP)
+
+  CLIENT = GraphQL::Client.new(schema: SCHEMA, execute: HTTP)
+
+  EMAIL_QUERY = CLIENT.parse <<-'GRAPHQL'
+    query($id: ID!) {
+      email(id: $id) {
+        from
+        to
+        subject
+        content {
+          html
+          text
+          source
+        }
+        createdAt
+        status
+        app {
+          id
+          name
+        }
+        opened
+        clicked
+        openEvents {
+          ip
+          userAgent {
+            family
+            version
+          }
+          os {
+            family
+            version
+          }
+          createdAt
+        }
+      }
+    }
+  GRAPHQL
+end
