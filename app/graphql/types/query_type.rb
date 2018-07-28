ce3c144358c0455acd417ec15ff6ccd40a2a8ad4
Matthew Landauer <mlandauer@gmail.com>
2018-07-29 03:31:15 +1000

    A few more descriptions and a little reformating

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,14 @@
 class Types::QueryType < Types::BaseObject
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
+  description "The query root for the Cuttlefish GraphQL API"
 
-  # TODO: Add authentication
-  # TODO: Add authorization
-  field :email, Types::EmailType, null: true, description: "A single email" do
-    argument :id, ID, required: true
-  end
+  field :email,
+        Types::EmailType,
+        null: true,
+        description: "A single email" do
+          argument :id, ID, required: true
+        end
 
   def email(id:)
     email = Delivery.find_by(id: id)
@@ -14,7 +16,10 @@ class Types::QueryType < Types::BaseObject
     email
   end
 
-  field :emails, [Types::EmailType], null: true, description: "All emails"
+  field :emails,
+        [Types::EmailType],
+        null: true,
+        description: "All emails"
 
   # TODO: Add pagination
   # TODO: Add authentication
@@ -24,12 +29,20 @@ class Types::QueryType < Types::BaseObject
     Pundit.policy_scope(context[:current_admin], Delivery)
   end
 
-  field :configuration, Types::ConfigurationType, null: false
+  field :configuration,
+        Types::ConfigurationType,
+        null: false,
+        description: "Application configuration settings"
+
   def configuration
     Rails.configuration
   end
 
-  field :viewer, Types::ViewerType, null: true
+  field :viewer,
+        Types::ViewerType,
+        null: true,
+        description: "The currently authenticated admin"
+
   def viewer
     context[:current_admin]
   end
