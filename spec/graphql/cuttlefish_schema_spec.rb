b91247e8042bd477b05d3ac3c0a726be99eb7c18
Matthew Landauer <mlandauer@gmail.com>
2018-08-06 11:33:15 +1000

    Restructure tests a little

--- spec/graphql/cuttlefish_schema_spec.rb
+++ spec/graphql/cuttlefish_schema_spec.rb
@@ -23,33 +23,30 @@ describe CuttlefishSchema do
 
   describe "email" do
     let(:query_string) { 'query($id: ID!) { email(id: $id) { id } }' }
+    let(:variables) { { id: delivery.id } }
 
-    context "with valid email id" do
-      let(:variables) { { id: delivery.id } }
+    it "should return a valid result" do
+      expect(result['data']['email']['id']).to eq delivery.id.to_s
+      expect(result['errors']).to be_nil
+    end
 
-      it "should return a valid result" do
-        expect(result['data']['email']['id']).to eq delivery.id.to_s
-        expect(result['errors']).to be_nil
+    context "with no current user" do
+      let(:context) { {} }
+
+      it "should return nil and error" do
+        expect(result['data']['email']).to be_nil
+        expect(result['errors'].length).to eq 1
+        expect(result['errors'][0]['message']).to eq "Need to be authenticated"
       end
+    end
 
-      context "with no current user" do
-        let(:context) { {} }
+    context "with a user in a different team" do
+      let(:admin) { FactoryBot.create(:admin, team: team_two) }
 
-        it "should return nil and error" do
-          expect(result['data']['email']).to be_nil
-          expect(result['errors'].length).to eq 1
-          expect(result['errors'][0]['message']).to eq "Need to be authenticated"
-        end
-      end
-
-      context "with a user in a different team" do
-        let(:admin) { FactoryBot.create(:admin, team: team_two) }
-
-        it "should reutrn nil and an error" do
-          expect(result['data']['email']).to be_nil
-          expect(result['errors'].length).to eq 1
-          expect(result['errors'][0]['message']).to eq "An object of type Email was hidden due to permissions"
-        end
+      it "should return nil and an error" do
+        expect(result['data']['email']).to be_nil
+        expect(result['errors'].length).to eq 1
+        expect(result['errors'][0]['message']).to eq "An object of type Email was hidden due to permissions"
       end
     end
 
