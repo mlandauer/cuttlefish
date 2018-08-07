38f12e705a91061baa00ddf0e2b5d845eeb8b61e
Matthew Landauer <mlandauer@gmail.com>
2018-08-08 04:03:28 +1000

    Add smtp_server to app in graphql

--- app/graphql/types/app_type.rb
+++ app/graphql/types/app_type.rb
@@ -2,4 +2,13 @@ class Types::AppType < Types::BaseObject
   description "An app in Cuttlefish"
   field :id, ID, null: false, description: "The database ID"
   field :name, String, null: true, description: "The name of the app"
+  field :smtp_server, Types::SmtpServerType, null: false, description: "Details needed to send email to Cuttlefish for this app"
+  def smtp_server
+    {
+      hostname: Rails.configuration.cuttlefish_domain,
+      port: Rails.configuration.cuttlefish_smtp_port,
+      username: object.smtp_username,
+      password: object.smtp_password
+     }
+  end
 end
---
+++ app/graphql/types/smtp_server_type.rb
@@ -0,0 +1,7 @@
+class Types::SmtpServerType < Types::BaseObject
+  description "Details needed to send email to the Cuttlefish SMTP server"
+  field :hostname, String, null: false, description: "The hostname"
+  field :port, Int, null: false, description: "The port"
+  field :username, String, null: false, description: "The username to authenticate"
+  field :password, String, null: false, description: "The password to authenticate"
+end
