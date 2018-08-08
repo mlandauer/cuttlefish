7385a2c5ab42466a8081865bc7c136ecfd92634c
Matthew Landauer <mlandauer@gmail.com>
2018-08-08 10:48:09 +1000

    Rename field to make it clearer

--- app/graphql/types/email_stats_type.rb
+++ app/graphql/types/email_stats_type.rb
@@ -1,6 +1,6 @@
 class Types::EmailStatsType < Types::BaseObject
   description "Statistics over a set of emails"
-  field :number_created, Int, null: false, description: "Number of emails created"
+  field :number_in_flight, Int, null: false, description: "Number of emails sent but not yet delivered or bounced"
   field :number_delivered, Int, null: false, description: "Number of emails delivered"
   field :number_soft_bounces, Int, null: false, description: "Number of emails that soft bounced"
   field :number_hard_bounces, Int, null: false, description: "Number of emails that hard bounced"
@@ -8,9 +8,8 @@ class Types::EmailStatsType < Types::BaseObject
   field :open_rate, Float, null: true, description: "Fraction of emails opened"
   field :click_rate, Float, null: true, description: "Fraction of emails with links that were clicked"
 
-  # TODO: This value could easily be confusing. Fix this
-  def number_created
-    status_counts.values.sum || 0
+  def number_in_flight
+    status_counts["sent"] || 0
   end
 
   def number_delivered
