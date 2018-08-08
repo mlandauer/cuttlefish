426281e13a694d22c977cbf7e5f63bc8df286cf4
Matthew Landauer <mlandauer@gmail.com>
2018-08-08 10:54:05 +1000

    Reorder to be less messy

--- app/graphql/types/email_type.rb
+++ app/graphql/types/email_type.rb
@@ -8,40 +8,43 @@ class Types::EmailType < Types::BaseObject
   field :id, ID, null: false, description: "The database ID of the email"
   field :from, String, null: true, description: "The email address which this email is from"
   field :to, String, null: false, description: "The destination email address"
+  field :subject, String, null: true, description: "The subject of this email"
+  field :content, Types::EmailContentType, null: true, description: "The full content of this email (if it is available)"
+  field :created_at, Types::DateTimeType, null: false, description: "When the email was received by Cuttlefish"
+  field :app, Types::AppType, null: true, description: "The app associated with this email"
+  field :status, Types::StatusType, null: false, description: "The status of this email"
+  field :opened, Boolean, null: false, description: "Whether this email was opened"
+  field :clicked, Boolean, null: false, description: "Whether this email was clicked"
+  # TODO: Rename to delivery_events
+  field :logs, [Types::LogType], null: false, description: "A list of delivery events for this email"
+  field :open_events, [Types::OpenEventType], null: false, description: "A list of open events for this email"
+  field :click_events, [Types::ClickEventType], null: false, description: "A list of click events for this email"
+
   def to
     address.text
   end
 
-  field :subject, String, null: true, description: "The subject of this email"
   def subject
     email.subject
   end
 
-  field :content, Types::EmailContentType, null: true, description: "The full content of this email (if it is available)"
   def content
     if object.data
       { text: object.text_part, html: object.html_part, source: object.data }
     end
   end
-  field :created_at, Types::DateTimeType, null: false, description: "When the email was received by Cuttlefish"
 
-  field :app, Types::AppType, null: true, description: "The app associated with this email"
-  field :status, Types::StatusType, null: false, description: "The status of this email"
-  field :opened, Boolean, null: false, description: "Whether this email was opened"
   def opened
     object.opened?
   end
-  field :clicked, Boolean, null: false, description: "Whether this email was clicked"
+
   def clicked
     delivery_links.any?{|delivery_link| delivery_link.clicked?}
   end
-  # TODO: Rename to delivery_events
-  field :logs, [Types::LogType], null: false, description: "A list of delivery events for this email"
+
   def logs
     object.postfix_log_lines
   end
-  field :open_events, [Types::OpenEventType], null: false, description: "A list of open events for this email"
-  field :click_events, [Types::ClickEventType], null: false, description: "A list of click events for this email"
 
   private
 
