b488c645b48617cd287089cc8f6ca8350c3264ff
Matthew Landauer <mlandauer@gmail.com>
2018-08-08 10:52:48 +1000

    Add TODO

--- app/graphql/types/status_type.rb
+++ app/graphql/types/status_type.rb
@@ -1,7 +1,8 @@
 class Types::StatusType < Types::BaseEnum
   description "The delivery status of an email"
   value "not_sent", "Not sent because it's on the blacklist"
-  value "sent", "Sent but not yet definitely delivered"
+  # TODO: Rename this to in_flight to be clearer
+  value "sent", "Sent but not yet delivered or bounced"
   value "delivered", "Delivered to its destination"
   value "soft_bounce", "A temporary delivery problem"
   value "hard_bounce", "A permanent delivery problem"
