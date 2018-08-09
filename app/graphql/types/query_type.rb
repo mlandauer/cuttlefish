34b948b6f0c93bdd33ca1f7d95dcd0cd78aab447
Matthew Landauer <mlandauer@gmail.com>
2018-08-10 06:12:57 +1000

    Reorder only

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,20 +3,9 @@ class Types::QueryType < Types::BaseObject
   # They will be entry points for queries on your schema.
   description "The query root for the Cuttlefish GraphQL API"
 
-  guard ->(object, args, context) {
-    # We always need to be authenticated
-    !context[:current_admin].nil?
-  }
-
   field :email, Types::EmailType, null: true do
     argument :id, ID, required: true, description: "ID of Email to find"
     description "Find a single Email"
-  end
-
-  def email(id:)
-    email = Delivery.find_by(id: id)
-    raise GraphQL::ExecutionError, "Email doesn't exist" if email.nil?
-    email
   end
 
   field :emails, Types::EmailConnectionType, connection: false, null: true do
@@ -28,6 +17,29 @@ class Types::QueryType < Types::BaseObject
     argument :limit, Int, required: false, description: "For pagination: sets maximum number of items returned"
     argument :offset, Int, required: false, description: "For pagination: sets offset"
     description "A list of Emails that this admin has access to. Most recent emails come first."
+  end
+
+  field :apps, [Types::AppType], null: true do
+    description "A list of Apps that this admin has access to, sorted alphabetically by name."
+  end
+
+  field :configuration, Types::ConfigurationType, null: false do
+    description "Application configuration settings"
+  end
+
+  field :viewer, Types::ViewerType, null: true do
+    description "The currently authenticated admin"
+  end
+
+  guard ->(object, args, context) {
+    # We always need to be authenticated
+    !context[:current_admin].nil?
+  }
+
+  def email(id:)
+    email = Delivery.find_by(id: id)
+    raise GraphQL::ExecutionError, "Email doesn't exist" if email.nil?
+    email
   end
 
   # TODO: Switch over to more relay-like pagination
@@ -45,24 +57,12 @@ class Types::QueryType < Types::BaseObject
     { all: emails, order: "created_at DESC", limit: limit, offset: offset }
   end
 
-  field :apps, [Types::AppType], null: true do
-    description "A list of Apps that this admin has access to, sorted alphabetically by name."
-  end
-
   def apps
     Pundit.policy_scope(context[:current_admin], App).order(:name)
   end
 
-  field :configuration, Types::ConfigurationType, null: false do
-    description "Application configuration settings"
-  end
-
   def configuration
     Rails.configuration
-  end
-
-  field :viewer, Types::ViewerType, null: true do
-    description "The currently authenticated admin"
   end
 
   def viewer
