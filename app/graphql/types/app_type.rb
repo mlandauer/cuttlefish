7a8547b069a9b25855cb9ae8f87c9281c0d9a34d
Matthew Landauer <mlandauer@gmail.com>
2018-08-11 15:24:08 +1000

    Add a couple more fields to App result

--- app/graphql/types/app_type.rb
+++ app/graphql/types/app_type.rb
@@ -4,7 +4,9 @@ class Types::AppType < Types::BaseObject
   field :id, ID, null: false, description: "The database ID"
   field :name, String, null: true, description: "The name of the app"
   field :smtp_server, Types::SmtpServerType, null: false, description: "Details needed to send email to Cuttlefish for this app"
-
+  field :cuttlefish, Boolean, null: false, description: "Whether this is the app used internally by cuttlefish for sending out its own emails"
+  field :dkim_enabled, Boolean, null: false, description: "Whether DKIM is enabled for this app"
+  
   guard ->(object, args, context) {
     AppPolicy.new(context[:current_admin], object.object).show?
   }
