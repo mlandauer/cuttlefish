8259c648b554a45b20994d2d0f1f344e1a662498
James Polley <jp@jamezpolley.com>
2018-09-02 18:07:53 +1000

    Need to create symlink for .env

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,7 @@ namespace :deploy do
       "#{release_path}/config/database.yml"                => "#{shared_path}/database.yml",
       "#{release_path}/db/emails"                          => "#{shared_path}/emails",
       "#{release_path}/db/archive"                         => "#{shared_path}/archive",
+      "#{release_path}/.env"                               => "#{shared_path}/.env",
     }
     # Copy across the example database configuration file if there isn't already one
     run "test -f #{shared_path}/database.yml || cp #{release_path}/config/database.yml #{shared_path}/database.yml"
