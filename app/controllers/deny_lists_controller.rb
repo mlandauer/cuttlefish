8aec217a2ca6cca1198cadb92908966c17bdc017
Matthew Landauer <mlandauer@gmail.com>
2018-09-04 12:01:34 +1000

    Rename controller

--- app/controllers/deny_lists_controller.rb
+++ app/controllers/deny_lists_controller.rb
@@ -1,4 +1,4 @@
-class BlackListsController < ApplicationController
+class DenyListsController < ApplicationController
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
