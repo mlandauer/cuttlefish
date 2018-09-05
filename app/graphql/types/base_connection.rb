749c7cd064f67f2067dbee2444e9b8623c425f21
Matthew Landauer <mlandauer@gmail.com>
2018-09-05 20:43:02 +1000

    Extract base class for connections

---
+++ app/graphql/types/base_connection.rb
@@ -0,0 +1,16 @@
+class Types::BaseConnection < GraphQL::Schema::Object
+  field :total_count, Integer, null: false, description: "The total count of items"
+  field :nodes, [Types::Email], null: true, description: "A list of nodes"
+
+  def total_count
+    object[:all].count
+  end
+
+  def nodes
+    limit = [object[:limit], MAX_LIMIT].min
+    object[:all].order(object[:order]).offset(object[:offset]).limit(limit)
+  end
+
+  # Limit can never be bigger than 50
+  MAX_LIMIT = 50
+end
--- app/graphql/types/email_connection.rb
+++ app/graphql/types/email_connection.rb
@@ -1,22 +1,8 @@
-class Types::EmailConnection < GraphQL::Schema::Object
+class Types::EmailConnection < Types::BaseConnection
   description "A list of Emails"
-  field :total_count, Integer, null: false, description: "The total count of items"
-  field :nodes, [Types::Email], null: true, description: "A list of nodes"
   field :statistics, Types::EmailStats, null: false, description: "Statistics over emails (ignoring pagination)"
-
-  def total_count
-    object[:all].count
-  end
-
-  def nodes
-    limit = [object[:limit], MAX_LIMIT].min
-    object[:all].order(object[:order]).offset(object[:offset]).limit(limit)
-  end
 
   def statistics
     object[:all]
   end
-
-  # Limit can never be bigger than 50
-  MAX_LIMIT = 50
 end
