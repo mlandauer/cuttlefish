7ee93c2570ba003a449ca8a16cfedb30f2092e95
Matthew Landauer <mlandauer@gmail.com>
2018-09-07 04:00:55 +1000

    Think we can get away without record_of_cause? method now

--- app/models/deny_list.rb
+++ app/models/deny_list.rb
@@ -3,10 +3,6 @@ class DenyList < ActiveRecord::Base
   belongs_to :address
   belongs_to :caused_by_delivery, class_name: "Delivery"
 
-  def has_record_of_cause?
-    caused_by_delivery.present? && caused_by_delivery.subject.present?
-  end
-
   def caused_by_postfix_log_line
     caused_by_delivery.postfix_log_lines.first if caused_by_delivery
   end
--- spec/models/deny_list_spec.rb
+++ spec/models/deny_list_spec.rb
@@ -1,33 +1,4 @@
 require 'spec_helper'
 
 describe DenyList do
-  describe "#has_record_of_deny_list_cause?" do
-    context "when the caused_by_delivery is missing" do
-      let(:deny_list) { create(:deny_list, caused_by_delivery: nil ) }
-
-      it { expect(deny_list.has_record_of_cause?).to be false }
-    end
-
-    context "when the caused_by_delivery present but has no subject" do
-      let(:deny_list) do
-        delivery = create(:delivery, email: create(:email, subject: nil))
-        create(:deny_list, caused_by_delivery: delivery)
-      end
-
-      it { expect(deny_list.has_record_of_cause?).to be false }
-    end
-
-    context "when the caused_by_delivery present and has a subject" do
-      let(:delivery) { create(:delivery) }
-      let(:deny_list) { create(:deny_list, caused_by_delivery: delivery) }
-
-      before do
-        allow(delivery).to receive(:subject).and_return("foo")
-      end
-
-      it do
-        expect(deny_list.has_record_of_cause?).to be true
-      end
-    end
-  end
 end
