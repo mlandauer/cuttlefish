c913823731c90549dfabf1724dd42e505f52770e
Matthew Landauer <mlandauer@gmail.com>
2018-09-10 07:57:09 +1000

    Rename spec to match name of thing being tested

--- spec/lib/hash_id_spec.rb
+++ spec/lib/hash_id_spec.rb
@@ -5,7 +5,6 @@ describe HashId do
     expect(HashId.hash("15")).to eq "29a6fc331a2dc6ebe86a055b91dfb19f6537f6c4"
   end
 
-
   describe ".valid?" do
     it { expect(HashId.valid?("15", "29a6fc331a2dc6ebe86a055b91dfb19f6537f6c4")).to be_truthy }
     it { expect(HashId.valid?("15", "this hash is wrong")).to be_falsy }
