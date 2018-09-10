2190bd841d10d6a555d99582db01821e80278808
Matthew Landauer <mlandauer@gmail.com>
2018-09-11 02:23:22 +1000

    Add policy for destroying an admin - can't destroy yourself

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -4,9 +4,25 @@ class AdminPolicy < ApplicationPolicy
     true
   end
 
+  def show?
+    in_same_team?
+  end
+
+  def destroy?
+    !Rails.configuration.cuttlefish_read_only_mode &&
+      in_same_team? &&
+      user.id != record.id
+  end
+
   class Scope < Scope
     def resolve
       scope.where(team_id: user.team_id)
     end
+  end
+
+  private
+
+  def in_same_team?
+    user.team_id == record.team_id
   end
 end
--- spec/policies/admin_policy_spec.rb
+++ spec/policies/admin_policy_spec.rb
@@ -12,16 +12,28 @@ describe AdminPolicy do
     let(:user) { FactoryBot.create(:admin, team: team_one)}
     it { is_expected.to permit(:show) }
     it { is_expected.to permit(:index) }
+    it { is_expected.to permit(:destroy) }
 
     it { is_expected.not_to permit(:create) }
     it { is_expected.not_to permit(:new) }
     it { is_expected.not_to permit(:update)  }
     it { is_expected.not_to permit(:edit)    }
-    it { is_expected.not_to permit(:destroy) }
 
     it 'should be in scope' do
       admin
       expect(AdminPolicy::Scope.new(user, Admin).resolve).to include(admin)
+    end
+
+    context "in read only mode" do
+      before :each do
+        allow(Rails.configuration).to receive(:cuttlefish_read_only_mode) { true }
+      end
+      it { is_expected.not_to permit(:destroy) }
+    end
+
+    context "user and admin are the same" do
+      let(:user) { admin }
+      it { is_expected.not_to permit(:destroy) }      
     end
   end
 
