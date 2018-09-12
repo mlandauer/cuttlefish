31a8077766129f930a70ce8c96992efd02397d8c
Matthew Landauer <mlandauer@gmail.com>
2018-09-13 09:31:25 +1000

    Add tests for registration policy

---
+++ spec/policies/registration_policy_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe RegistrationPolicy do
+  subject { RegistrationPolicy.new(user, nil) }
+
+  context "normal user" do
+    let(:user) { FactoryBot.create(:admin)}
+
+    it { is_expected.to permit(:create) }
+    it { is_expected.to permit(:update)  }
+    it { is_expected.to permit(:edit) }
+    it { is_expected.to permit(:destroy)  }
+
+    context "in read only mode" do
+      before :each do
+        allow(Rails.configuration).to receive(:cuttlefish_read_only_mode) { true }
+      end
+      it { is_expected.not_to permit(:create) }
+      it { is_expected.not_to permit(:update)  }
+      it { is_expected.not_to permit(:edit) }
+      it { is_expected.not_to permit(:destroy)  }
+    end
+  end
+end
