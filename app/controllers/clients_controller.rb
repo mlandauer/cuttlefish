049fac20fb124f39819a96e5eef548b2cce16369
Matthew Landauer <mlandauer@gmail.com>
2018-09-17 17:37:06 +1000

    Extract template variable

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,5 @@
 class ClientsController < ApplicationController
   def index
+    @client_counts = policy_scope(Delivery).joins(:open_events).group(:ua_family).order("count_all desc").count
   end
 end
