b255c7d0c4b9fcb8052e14d85cce2c7e40b6aa1e
Matthew Landauer <mlandauer@gmail.com>
2018-09-17 11:13:33 +1000

    Add permissions query for blocked address

--- app/graphql/types/blocked_address.rb
+++ app/graphql/types/blocked_address.rb
@@ -2,6 +2,10 @@ class Types::BlockedAddress < GraphQL::Schema::Object
   field :id, ID, null: false, description: "The database ID"
   field :address, String, null: false, description: "Email address"
   field :because_of_delivery_event, Types::DeliveryEvent, null: true, description: "The delivery attempt that bounced that caused this address to be blocked"
+  field :permissions, Types::BlockedAddressPermissions, null: false, description: "Permissions for current admin for accessing and editing this blocked address" do
+    # Permissions should be always accessible even on apps that you can't show
+    guard ->(obj, args, ctx) { true }
+  end
 
   def address
     object.address.text
@@ -9,5 +13,9 @@ class Types::BlockedAddress < GraphQL::Schema::Object
 
   def because_of_delivery_event
     object.caused_by_postfix_log_line
+  end
+
+  def permissions
+    DenyListPolicy.new(context[:current_admin], object)
   end
 end
---
+++ app/graphql/types/blocked_address_permissions.rb
@@ -0,0 +1,5 @@
+class Types::BlockedAddressPermissions < GraphQL::Schema::Object
+  description "Permissions for current admin for accessing and editing a blocked address"
+
+  field :destroy, Boolean, null: false, method: :destroy?
+end
