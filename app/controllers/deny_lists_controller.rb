db8db7a9f51d3b67a68212bf7dcd5fa3eca9117e
Matthew Landauer <mlandauer@gmail.com>
2018-09-19 07:21:00 +1000

    Refactor deny list destroy action to use new service object. Also add a bit of flash feedback

--- app/controllers/deny_lists_controller.rb
+++ app/controllers/deny_lists_controller.rb
@@ -1,6 +1,4 @@
 class DenyListsController < ApplicationController
-  after_action :verify_authorized, except: :index
-
   def index
     @deny_lists = WillPaginate::Collection.create(params[:page] || 1, WillPaginate.per_page) do |pager|
       result = Cuttlefish::ApiClient.query(
@@ -14,9 +12,12 @@ class DenyListsController < ApplicationController
   end
 
   def destroy
-    deny_list = DenyList.find(params[:id])
-    authorize deny_list
-    deny_list.destroy
+    destroy_deny_list = DestroyDenyList.call(current_admin: current_admin, id: params[:id])
+    if destroy_deny_list.result
+      flash[:notice] = "#{destroy_deny_list.result.address.text} removed from deny list"
+    else
+      flash[:alert] = "Couldn't remove from deny list. You probably don't have the necessary permissions."
+    end
     redirect_back(fallback_location: deny_lists_url)
   end
 end
