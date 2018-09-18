22ffbef575581955a2ae639877a7b888241dd5ed
Matthew Landauer <mlandauer@gmail.com>
2018-09-18 20:12:12 +1000

    Add new service class

---
+++ app/services/destroy_deny_list.rb
@@ -0,0 +1,17 @@
+class DestroyDenyList < ApplicationService
+  def initialize(current_admin:, id:)
+    @current_admin = current_admin
+    @id = id
+  end
+
+  def call
+    deny_list = DenyList.find_by_id(id)
+    if deny_list && DenyListPolicy.new(current_admin, deny_list).destroy?
+      deny_list.destroy!
+    end
+  end
+
+  private
+
+  attr_reader :id, :current_admin
+end
---
+++ spec/services/destroy_deny_list_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe DestroyDenyList do
+  let(:team) { create(:team) }
+  let(:current_admin) { create(:admin, team: team) }
+  let(:deny_list) { create(:deny_list, team: team) }
+  let(:destroy_deny_list) { DestroyDenyList.call(current_admin: current_admin, id: deny_list.id) }
+
+  it "should remove a deny list entry" do
+    deny_list
+    expect { destroy_deny_list }.to change { DenyList.count }.by(-1)
+  end
+
+  it "should return the deleted entry" do
+    expect(destroy_deny_list.result).to eq deny_list
+  end
+
+  context "entry does not exist" do
+    before(:each) { deny_list.destroy! }
+
+    it "should return nil" do
+      expect(destroy_deny_list.result).to be_nil
+    end
+  end
+
+  context "does not have permission" do
+    let(:deny_list_policy) { double }
+    before :each do
+      expect(DenyListPolicy).to receive(:new) { deny_list_policy }
+      expect(deny_list_policy).to receive(:destroy?) { false }
+    end
+
+    it "should return nil" do
+      expect(destroy_deny_list.result).to be_nil
+    end
+  end
+end
