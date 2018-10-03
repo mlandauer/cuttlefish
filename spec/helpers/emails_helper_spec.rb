bffcf03634fb349a31df47afa53173ef7aca7928
Matthew Landauer <mlandauer@gmail.com>
2018-10-04 08:02:28 +1000

    Style fixes

--- spec/helpers/emails_helper_spec.rb
+++ spec/helpers/emails_helper_spec.rb
@@ -1,21 +1,43 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 describe EmailsHelper, type: :helper do
   describe ".bootstrap_status_class" do
-    it { expect(helper.bootstrap_status_class("delivered")).to eq "success"}
-    it { expect(helper.bootstrap_status_class("soft_bounce")).to eq "warning"}
-    it { expect(helper.bootstrap_status_class("hard_bounce")).to eq "error"}
-    it { expect(helper.bootstrap_status_class("sent")).to eq "success"}
-    it { expect {helper.bootstrap_status_class("foo") }.to raise_error("Unknown status") }
+    it { expect(helper.bootstrap_status_class("delivered")).to eq "success" }
+    it { expect(helper.bootstrap_status_class("soft_bounce")).to eq "warning" }
+    it { expect(helper.bootstrap_status_class("hard_bounce")).to eq "error" }
+    it { expect(helper.bootstrap_status_class("sent")).to eq "success" }
+    it {
+      expect { helper.bootstrap_status_class("foo") }.to raise_error(
+        "Unknown status"
+      )
+    }
   end
 
   describe ".delivered_label" do
-    it { expect(helper.delivered_label("delivered")).to eq '<span class="label label-success">Delivered</span>' }
-    it { expect(helper.delivered_label("soft_bounce")).to eq '<span class="label label-warning">Soft bounce</span>' }
-    it { expect(helper.delivered_label("hard_bounce")).to eq '<span class="label label-important">Hard bounce</span>' }
-    it { expect(helper.delivered_label("sent")).to eq '<span class="label label-success">Sent</span>' }
-    it { expect {helper.delivered_label("foo")}.to raise_error("Unknown status") }
+    it {
+      expect(helper.delivered_label("delivered")).to eq(
+        '<span class="label label-success">Delivered</span>'
+      )
+    }
+    it {
+      expect(helper.delivered_label("soft_bounce")).to eq(
+        '<span class="label label-warning">Soft bounce</span>'
+      )
+    }
+    it {
+      expect(helper.delivered_label("hard_bounce")).to eq(
+        '<span class="label label-important">Hard bounce</span>'
+      )
+    }
+    it {
+      expect(helper.delivered_label("sent")).to eq(
+        '<span class="label label-success">Sent</span>'
+      )
+    }
+    it {
+      expect { helper.delivered_label("foo") }.to raise_error("Unknown status")
+    }
   end
 end
--- spec/views/deliveries/show.json.erb_spec.rb
+++ spec/views/deliveries/show.json.erb_spec.rb
@@ -4,22 +4,26 @@ require "spec_helper"
 
 describe "deliveries/show.json.erb", type: :view do
   it do
-    app = mock_model(App,
+    app = mock_model(
+      App,
       id: 2,
       name: "Planning Alerts",
       url: "http://www.planningalerts.org.au/",
       custom_tracking_domain: "email.planningalerts.org.au",
       from_domain: "planningalerts.org.au"
     )
-    from_address = mock_model(Address,
+    from_address = mock_model(
+      Address,
       id: 12,
       text: "bounces@planningalerts.org.au"
     )
-    to_address = mock_model(Address,
+    to_address = mock_model(
+      Address,
       id: 13,
       text: "foo@gmail.com"
     )
-    email = mock_model(Email,
+    email = mock_model(
+      Email,
       id: 1753541,
       from_address: from_address,
       app: app,
@@ -27,41 +31,53 @@ describe "deliveries/show.json.erb", type: :view do
       message_id: "538ef46757549_443e4bb0f901893332@kedumba.mail",
       subject: "1 new planning application"
     )
-    link1 = mock_model(Link,
+    link1 = mock_model(
+      Link,
       id: 123,
       url: "http://www.planningalerts.org.au/alerts/abc1234/area"
     )
-    link2 = mock_model(Link,
+    link2 = mock_model(
+      Link,
       id: 321,
       url: "http://www.planningalerts.org.au/alerts/abc1234/unsubscribe"
     )
-    delivery_link1 = mock_model(DeliveryLink,
+    delivery_link1 = mock_model(
+      DeliveryLink,
       link: link1,
       click_events: []
     )
-    click_event = mock_model(ClickEvent,
-      user_agent: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0",
+    click_event = mock_model(
+      ClickEvent,
+      user_agent: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) " \
+                  "Gecko/20100101 Firefox/24.0",
       ip: "1.2.3.4",
       created_at: "2014-06-04T20:33:53.000+10:00"
     )
-    delivery_link2 = mock_model(DeliveryLink,
+    delivery_link2 = mock_model(
+      DeliveryLink,
       link: link2,
       click_events: [click_event]
     )
-    open_event = mock_model(OpenEvent,
-      user_agent: "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7 (via ggpht.com GoogleImageProxy)",
+    open_event = mock_model(
+      OpenEvent,
+      user_agent: "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.0.7) " \
+                  "Gecko/2009021910 Firefox/3.0.7 " \
+                  "(via ggpht.com GoogleImageProxy)",
       ip: "2.3.4.5",
       created_at: "2014-10-06T16:05:52.000+11:00"
     )
-    postfix_log_line = mock_model(PostfixLogLine,
+    postfix_log_line = mock_model(
+      PostfixLogLine,
       time: "2014-06-04T20:26:53.000+10:00",
       relay: "gmail-smtp-in.l.google.com[173.194.79.26]:25",
       delay: "1.7",
       delays: "0.05/0/0.58/1",
       dsn: "2.0.0",
-      extended_status: "sent (250 2.0.0 OK 1401877617 bh2si4687161pbb.204 - gsmtp)"
+      extended_status:
+        "sent (250 2.0.0 OK 1401877617 bh2si4687161pbb.204 - gsmtp)"
     )
-    delivery = mock_model(Delivery,
+    delivery = mock_model(
+      Delivery,
       id: 5,
       email: email,
       address: to_address,
@@ -78,7 +94,7 @@ describe "deliveries/show.json.erb", type: :view do
     assign(:delivery, delivery)
     render
 
-    expect(JSON.parse(rendered, symbolize_names: true)).to eq ({
+    expect(JSON.parse(rendered, symbolize_names: true)).to eq(
       id: 5,
       from_address: {
         id: 12,
@@ -106,7 +122,9 @@ describe "deliveries/show.json.erb", type: :view do
         open_tracked: true,
         open_events: [
           {
-            user_agent: "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7 (via ggpht.com GoogleImageProxy)",
+            user_agent:
+              "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.0.7) " \
+              "Gecko/2009021910 Firefox/3.0.7 (via ggpht.com GoogleImageProxy)",
             referer: nil,
             ip: "2.3.4.5",
             created_at: "2014-10-06T16:05:52.000+11:00"
@@ -116,14 +134,15 @@ describe "deliveries/show.json.erb", type: :view do
           {
             id: 123,
             url: "http://www.planningalerts.org.au/alerts/abc1234/area",
-            click_events: [ ]
+            click_events: []
           },
           {
             id: 321,
             url: "http://www.planningalerts.org.au/alerts/abc1234/unsubscribe",
             click_events: [
               {
-                user_agent: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0",
+                user_agent: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) " \
+                            "Gecko/20100101 Firefox/24.0",
                 referer: nil,
                 ip: "1.2.3.4",
                 created_at: "2014-06-04T20:33:53.000+10:00"
@@ -139,10 +158,11 @@ describe "deliveries/show.json.erb", type: :view do
             delay: "1.7",
             delays: "0.05/0/0.58/1",
             dsn: "2.0.0",
-            extended_status: "sent (250 2.0.0 OK 1401877617 bh2si4687161pbb.204 - gsmtp)"
+            extended_status:
+              "sent (250 2.0.0 OK 1401877617 bh2si4687161pbb.204 - gsmtp)"
           }
         ]
       }
-    })
+    )
   end
 end
