b4ac0fdf9e8b4283d26e1b5e407c48c97dbb9dc6
Matthew Landauer <mlandauer@gmail.com>
2018-10-05 10:24:22 +1000

    Inline whole class

--- app/services/email_services/send.rb
+++ app/services/email_services/send.rb
@@ -14,7 +14,32 @@ module EmailServices
     private
 
     def send(delivery)
-      OutgoingDelivery.new(delivery).send
+      return unless delivery.send?
+
+      # TODO: Replace use of Net::SMTP with deliver! as part of mail gem
+      Net::SMTP.start(
+        Rails.configuration.postfix_smtp_host,
+        Rails.configuration.postfix_smtp_port
+      ) do |smtp|
+        response = smtp.send_message(
+          Filters::Master.new(delivery).filter(delivery.data),
+          delivery.return_path,
+          [delivery.to]
+        )
+        delivery.update_attributes(
+          open_tracked: delivery.open_tracking_enabled?,
+          postfix_queue_id:
+            extract_postfix_queue_id_from_smtp_message(response.message),
+          sent: true
+        )
+      end
+    end
+
+    # When a message is sent via the Postfix MTA it returns the queue id
+    # in the SMTP message. Extract this
+    def extract_postfix_queue_id_from_smtp_message(message)
+      m = message.match(/250 2.0.0 Ok: queued as (\w+)/)
+      m[1] if m
     end
 
     attr_reader :email
