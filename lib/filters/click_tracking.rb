09ea9f619ef9528dfb1d1467bc2edf2658a244d9
Matthew Landauer <mlandauer@gmail.com>
2018-10-06 10:23:26 +1000

    Merge base class back in

--- lib/filters/click_tracking.rb
+++ lib/filters/click_tracking.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
 module Filters
-  class ClickTracking < Filters::Tracking
+  class ClickTracking < Filters::Mail
     include Rails.application.routes.url_helpers
 
-    attr_accessor :delivery_id, :enabled
+    attr_accessor :delivery_id, :enabled,
+                  :tracking_domain, :using_custom_tracking_domain
 
     def initialize(delivery_id:, enabled:,
                    tracking_domain:, using_custom_tracking_domain:)
       @delivery_id = delivery_id
       @enabled = enabled
-      super(
-        tracking_domain: tracking_domain,
-        using_custom_tracking_domain: using_custom_tracking_domain
-      )
+      @tracking_domain = tracking_domain
+      @using_custom_tracking_domain = using_custom_tracking_domain
     end
 
     def rewrite_url(url)
@@ -41,6 +40,16 @@ module Filters
       else
         input
       end
+    end
+
+    # Hostname to use for the open tracking image or rewritten link
+    def host
+      Rails.env.development? ? "localhost:3000" : tracking_domain
+    end
+
+    # Whether to use ssl for the open tracking image or rewritten link
+    def protocol
+      using_custom_tracking_domain || Rails.env.development? ? "http" : "https"
     end
   end
 end
