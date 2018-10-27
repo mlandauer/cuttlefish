601d0b033ec3875518eb7d30b1271a9f6b57c8ea
Matthew Landauer <mlandauer@gmail.com>
2018-10-28 03:44:48 +1100

    Simplify

--- app/services/admin_services/destroy.rb
+++ app/services/admin_services/destroy.rb
@@ -8,15 +8,11 @@ module AdminServices
     end
 
     def call
-      admin = Admin.find_by_id(id)
-      if admin.nil?
-        raise ActiveRecord::RecordNotFound
-      elsif !AdminPolicy.new(current_admin, admin).destroy?
-        raise Pundit::NotAuthorizedError
-      else
-        success!
-        admin.destroy
-      end
+      admin = Admin.find(id)
+      Pundit.authorize(current_admin, admin, :destroy?)
+      admin.destroy
+      success!
+      admin
     end
 
     private
