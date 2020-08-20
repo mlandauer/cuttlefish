c8841fff0bc7ac594862099fe03ff70e48c1a1b8
Matthew Landauer <mlandauer@gmail.com>
2020-08-20 10:00:19 +1000

    Move header parsing out of smtp server

--- lib/cuttlefish_smtp_server.rb
+++ lib/cuttlefish_smtp_server.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.expand_path File.join(File.dirname(__FILE__), "create_email_worker")
+require File.expand_path File.join(File.dirname(__FILE__), "parse_headers_create_email_worker")
 require File.expand_path File.join(File.dirname(__FILE__), "email_data_cache")
 require "ostruct"
 require "eventmachine"
@@ -165,36 +165,12 @@ class CuttlefishSmtpConnection < EM::P::SmtpServer
     end
   end
 
-  IGNORE_DENY_LIST_HEADER = "X-Cuttlefish-Ignore-Deny-List"
-
   def receive_message
     current.received = true
     current.completed_at = Time.now
 
     # TODO: No need to capture current.sender, current.received,
     # current.completed_at because we're not passing it on
-
-    # Now check for special headers
-    m = Mail.new(current.data)
-    h = m.header[IGNORE_DENY_LIST_HEADER]
-    ignore_deny_list = (!h.nil? && h.value == "true")
-
-    # Remove header
-    m.header[IGNORE_DENY_LIST_HEADER] = nil if m.header[IGNORE_DENY_LIST_HEADER]
-
-    # Check for metadata headers that start with "X-Cuttlefish-Metadata-"
-    meta_values = {}
-    names = []
-    m.header_fields.each do |field|
-      match = field.name.match(/^X-Cuttlefish-Metadata-(.*)$/)
-      if match
-        meta_values[match[1]] = field.value
-        names << field.name
-      end
-    end
-
-    # Remove headers
-    names.each { |name| m.header[name] = nil }
 
     # Store content of email in a temporary file
     # Note that this depends on having access to the same filesystem as
@@ -203,17 +179,15 @@ class CuttlefishSmtpConnection < EM::P::SmtpServer
     # true in the future
     # Using Tempfile.create rather than Tempfile.new so that the tmp file is
     # not automatically deleted through garbage collection
-    file = Tempfile.create("cuttlefish")
-    file.write(m.to_s)
+    file = Tempfile.create("cuttlefish", encoding: "ascii-8bit")
+    file.write(current.data)
     file.close
 
     # Note the worker will delete the temporary file when it's done
-    CreateEmailWorker.perform_async(
+    ParseHeadersCreateEmailWorker.perform_async(
       current.recipients,
       file.path,
-      current.app_id,
-      ignore_deny_list,
-      meta_values
+      current.app_id
     )
 
     # Preserve the app_id as we are already authenticated
