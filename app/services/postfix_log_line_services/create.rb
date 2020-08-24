9fb273f438befd425511b7e934d9cffb90f30f52
Matthew Landauer <mlandauer@gmail.com>
2020-08-24 15:32:31 +1000

    Extract method

--- app/services/postfix_log_line_services/create.rb
+++ app/services/postfix_log_line_services/create.rb
@@ -11,28 +11,30 @@ module PostfixLogLineServices
       PostfixLogLine.transaction do
         log_line = PostfixLogLine.create_from_line(line)
         # Check if an email needs to be deny listed
-        if log_line && log_line.status == "hard_bounce"
-          # We don't want to save duplicates
-          if DenyList.find_by(
-            team_id: log_line.delivery.app.team_id,
-            address: log_line.delivery.address
-          ).nil?
-            # It is possible for the team_id to be nil if it's a mail from the
-            # cuttlefish "app" that is causing a hard bounce. For the time being
-            # let's just ignore those mails and not try to add them to the deny
-            # list because if we do they will cause an error
-            # TODO: Fix this properly. What's here now is just a temporary
-            # workaround
-            if log_line.delivery.app.team_id
-              DenyList.create(
-                team_id: log_line.delivery.app.team_id,
-                address: log_line.delivery.address,
-                caused_by_delivery: log_line.delivery
-              )
-            end
-          end
-        end
+        add_to_deny_list(log_line.delivery) if log_line && log_line.status == "hard_bounce"
       end
+    end
+
+    def add_to_deny_list(delivery)
+      # It is possible for the team_id to be nil if it's a mail from the
+      # cuttlefish "app" that is causing a hard bounce. For the time being
+      # let's just ignore those mails and not try to add them to the deny
+      # list because if we do they will cause an error
+      # TODO: Fix this properly. What's here now is just a temporary
+      # workaround
+      return if delivery.app.team_id.nil?
+
+      # We don't want to save duplicates
+      return if DenyList.find_by(
+        team_id: delivery.app.team_id,
+        address: delivery.address
+      )
+
+      DenyList.create(
+        team_id: delivery.app.team_id,
+        address: delivery.address,
+        caused_by_delivery: delivery
+      )
     end
 
     private
