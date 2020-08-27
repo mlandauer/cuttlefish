7ccc5af4cbf3720c68005b0514d49e472261d3f2
Matthew Landauer <mlandauer@gmail.com>
2020-08-27 13:14:33 +1000

    Make sure we're not actually creating multiple apps with the factory

--- spec/views/deliveries/show.json.erb_spec.rb
+++ spec/views/deliveries/show.json.erb_spec.rb
@@ -36,51 +36,6 @@ describe "deliveries/show.json.erb", type: :view do
       data_hash: "aa126db79482378ce17b441347926570228f12ef"
     )
 
-    link1 = create(
-      :link,
-      id: 123,
-      url: "http://www.planningalerts.org.au/alerts/abc1234/area"
-    )
-    link2 = create(
-      :link,
-      id: 321,
-      url: "http://www.planningalerts.org.au/alerts/abc1234/unsubscribe"
-    )
-    delivery_link1 = create(
-      :delivery_link,
-      link: link1
-    )
-    click_event = create(
-      :click_event,
-      user_agent: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) " \
-                  "Gecko/20100101 Firefox/24.0",
-      ip: "1.2.3.4",
-      referer: nil,
-      created_at: "2014-06-04T20:33:53.000+10:00"
-    )
-    delivery_link2 = create(
-      :delivery_link,
-      link: link2,
-      click_events: [click_event]
-    )
-    open_event = create(
-      :open_event,
-      user_agent: "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.0.7) " \
-                  "Gecko/2009021910 Firefox/3.0.7 " \
-                  "(via ggpht.com GoogleImageProxy)",
-      ip: "2.3.4.5",
-      created_at: "2014-10-06T16:05:52.000+11:00"
-    )
-    postfix_log_line = create(
-      :postfix_log_line,
-      time: "2014-06-04T20:26:53.000+10:00",
-      relay: "gmail-smtp-in.l.google.com[173.194.79.26]:25",
-      delay: "1.7",
-      delays: "0.05/0/0.58/1",
-      dsn: "2.0.0",
-      extended_status:
-        "sent (250 2.0.0 OK 1401877617 bh2si4687161pbb.204 - gsmtp)"
-    )
     delivery = create(
       :delivery,
       id: 5,
@@ -89,15 +44,61 @@ describe "deliveries/show.json.erb", type: :view do
       created_at: "2014-06-04T20:26:51.000+10:00",
       updated_at: "2014-06-04T20:26:55.000+10:00",
       sent: true,
-      status: "delivered",
       open_tracked: true,
-      delivery_links: [delivery_link1, delivery_link2],
-      open_events: [open_event],
-      postfix_log_lines: [postfix_log_line],
       postfix_queue_id: "38B72370AC41"
     )
     create(:meta_value, email: email, key: "foo", value: "bar")
     create(:meta_value, email: email, key: "wibble", value: "wobble")
+    create(
+      :delivery_link,
+      delivery: delivery,
+      link: create(
+        :link,
+        id: 123,
+        url: "http://www.planningalerts.org.au/alerts/abc1234/area"
+      )
+    )
+    delivery_link = create(
+      :delivery_link,
+      delivery: delivery,
+      link: create(
+        :link,
+        id: 321,
+        url: "http://www.planningalerts.org.au/alerts/abc1234/unsubscribe"
+      )
+    )
+    create(
+      :click_event,
+      delivery_link: delivery_link,
+      user_agent: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) " \
+                  "Gecko/20100101 Firefox/24.0",
+      ip: "1.2.3.4",
+      referer: nil,
+      created_at: "2014-06-04T20:33:53.000+10:00"
+    )
+    create(
+      :open_event,
+      delivery: delivery,
+      user_agent: "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.0.7) " \
+                  "Gecko/2009021910 Firefox/3.0.7 " \
+                  "(via ggpht.com GoogleImageProxy)",
+      ip: "2.3.4.5",
+      created_at: "2014-10-06T16:05:52.000+11:00"
+    )
+    create(
+      :postfix_log_line,
+      delivery: delivery,
+      time: "2014-06-04T20:26:53.000+10:00",
+      relay: "gmail-smtp-in.l.google.com[173.194.79.26]:25",
+      delay: "1.7",
+      delays: "0.05/0/0.58/1",
+      dsn: "2.0.0",
+      extended_status:
+        "sent (250 2.0.0 OK 1401877617 bh2si4687161pbb.204 - gsmtp)"
+    )
+
+    delivery.update_column(:status, "delivered")
+    delivery.reload
 
     assign(:delivery, delivery)
     render
