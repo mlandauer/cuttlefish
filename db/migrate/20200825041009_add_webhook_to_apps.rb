493677d7f2b14efa5384aad2c44d7d2ab0231f23
Matthew Landauer <mlandauer@gmail.com>
2020-08-27 12:46:52 +1000

    Don't validate when updating the webhook key in migrations

--- db/migrate/20200825041009_add_webhook_to_apps.rb
+++ db/migrate/20200825041009_add_webhook_to_apps.rb
@@ -5,7 +5,10 @@ class AddWebhookToApps < ActiveRecord::Migration[5.2]
     App.reset_column_information
     App.find_each do |app|
       app.set_webhook_key
-      app.save!
+      # If any of the DNS records have changed since the app was
+      # initially setup the validation will fail. We don't want this
+      # to happen. So, ignore validation.
+      app.save!(validate: false)
     end
     change_column_null :apps, :webhook_key, false
   end
