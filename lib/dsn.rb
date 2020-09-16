b824972b5984280a5a7d7e23c1b2f20ba9f5026d
Matthew Landauer <mlandauer@gmail.com>
2020-09-16 10:58:56 +1000

    Refactor underlying data structure

--- lib/dsn.rb
+++ lib/dsn.rb
@@ -4,30 +4,33 @@ require "csv"
 
 module DSN
   def self.read_csv
+    @read_csv ||= read_csv_no_caching
+  end
+
+  def self.read_csv_no_caching
     # This CSV file has been downloaded from the registry of DSN codes at
     # https://www.iana.org/assignments/smtp-enhanced-status-codes/smtp-enhanced-status-codes.xhtml
-    # Note that we're also caching result so we don't need to read the file twice
-    @read_csv ||= CSV.read("lib/smtp-enhanced-status-codes-3.csv", headers: true).map do |row|
-      {
-        code: row["Code"],
-        short_description: row["Sample Text"],
-        long_description: row["Description"]
-      }
+    r = CSV.read("lib/smtp-enhanced-status-codes-3.csv", headers: true).map do |row|
+      [
+        row["Code"],
+        {
+          short_description: row["Sample Text"],
+          long_description: row["Description"]
+        }
+      ]
     end
+    Hash[r]
   end
 
   def self.hard_bounce_data
-    read_csv.map do |row|
-      row[:code] = row[:code].tr("X", "5")
-      row
-    end
+    read_csv.transform_keys { |code| code.tr("X", "5") }
   end
 
   def self.hard_bounce_codes
-    hard_bounce_data.map { |row| row[:code] }
+    hard_bounce_data.keys
   end
 
   def self.hard_bounce_codes_short_descriptions
-    Hash[hard_bounce_data.map { |row| [row[:code], row[:short_description]] }]
+    hard_bounce_data.transform_values { |v| v[:short_description] }
   end
 end
