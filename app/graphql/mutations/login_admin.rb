3d897ef89fe2b1259bdc4dc0fb625c457743a6c5
Matthew Landauer <mlandauer@gmail.com>
2020-10-08 13:49:30 +1100

    Extract service

--- app/graphql/mutations/login_admin.rb
+++ app/graphql/mutations/login_admin.rb
@@ -10,16 +10,9 @@ module Mutations
     field :errors, [Types::UserError], null: false
 
     def resolve(email:, password:)
-      # For the time being just implement everything here
-      # TODO: Move stuff to a service class
-      admin = Admin.find_by(email: email)
-      # TODO: Also do the hashing (and throw away the result) if admin is nil
-      if admin&.valid_password?(password)
-        # Token has expiry time of 1 hour
-        exp = Time.now.to_i + 3600
-        # Keeping the claims to an absolute minimum for the time being
-        payload = { admin_id: admin.id, exp: exp }
-        token = JWT.encode payload, ENV["JWT_SECRET"], "HS512"
+      login_admin = AdminServices::Login.call(email: email, password: password)
+      if login_admin.success?
+        admin, token = login_admin.result
         { token: token, admin: admin, errors: [] }
       else
         { errors: [{ message: "Invalid email or password", type: "invalid" }] }
---
+++ app/services/admin_services/login.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module AdminServices
+  class Login < ApplicationService
+    def initialize(email:, password:)
+      super()
+      @email = email
+      @password = password
+    end
+
+    def call
+      admin = Admin.find_by(email: email)
+      # TODO: Also do the hashing (and throw away the result) if admin is nil
+      if admin&.valid_password?(password)
+        token = JWT.encode(jwt_payload(admin), jwt_secret, "HS512")
+        success!
+        [admin, token]
+      else
+        fail!
+      end
+    end
+
+    # Token has expiry time of 1 hour
+    def exp
+      Time.now.to_i + 3600
+    end
+
+    # Keeping the claims to an absolute minimum for the time being
+    def jwt_payload(admin)
+      { admin_id: admin.id, exp: exp }
+    end
+
+    def jwt_secret
+      ENV["JWT_SECRET"]
+    end
+
+    private
+
+    attr_reader :email, :password
+  end
+end
