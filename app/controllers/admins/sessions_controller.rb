61203602615081c2f712b95d6b697b9ce361c05a
Matthew Landauer <mlandauer@gmail.com>
2020-10-12 10:09:25 +1100

    Bring in devise code for reference

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -5,6 +5,15 @@ module Admins
     layout "login"
     before_action :check_first_user, only: :new
 
+    # POST /resource/sign_in
+    def create
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    end
+
     private
 
     def check_first_user
