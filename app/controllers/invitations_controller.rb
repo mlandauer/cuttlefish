590d1c11762ff6cf062b453d6f2abac127abeb8e
Matthew Landauer <mlandauer@gmail.com>
2020-10-15 13:36:55 +1100

    Simplify code based on our situation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -19,19 +19,10 @@ class InvitationsController < Devise::InvitationsController
     @data = result.data
 
     self.resource = invite_resource
-    resource_invited = resource.errors.empty?
 
-    yield resource if block_given?
-
-    if resource_invited
-      if is_flashing_format? && resource.invitation_sent_at
-        set_flash_message :notice, :send_instructions, email: resource.email
-      end
-      if method(:after_invite_path_for).arity == 1
-        respond_with resource, location: after_invite_path_for(current_inviter)
-      else
-        respond_with resource, location: after_invite_path_for(current_inviter, resource)
-      end
+    if resource.errors.empty?
+      set_flash_message :notice, :send_instructions, email: resource.email
+      respond_with resource, location: after_invite_path_for(current_inviter)
     else
       respond_with_navigational(resource) { render :new }
     end
