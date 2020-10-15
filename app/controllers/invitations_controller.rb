e0dc72177b95e7230f4462597ecf7b2a954f136d
Matthew Landauer <mlandauer@gmail.com>
2020-10-15 13:28:25 +1100

    Inline code from parent class for easy reference

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -17,7 +17,24 @@ class InvitationsController < Devise::InvitationsController
     authorize :invitation
     result = api_query
     @data = result.data
-    super
+
+    self.resource = invite_resource
+    resource_invited = resource.errors.empty?
+
+    yield resource if block_given?
+
+    if resource_invited
+      if is_flashing_format? && resource.invitation_sent_at
+        set_flash_message :notice, :send_instructions, email: resource.email
+      end
+      if method(:after_invite_path_for).arity == 1
+        respond_with resource, location: after_invite_path_for(current_inviter)
+      else
+        respond_with resource, location: after_invite_path_for(current_inviter, resource)
+      end
+    else
+      respond_with_navigational(resource) { render :new }
+    end
   end
 
   def update
