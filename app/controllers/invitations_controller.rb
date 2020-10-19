b47e8718332aaffc2cfb70ef3f06e5cad5844eaf
Matthew Landauer <mlandauer@gmail.com>
2020-10-20 10:32:54 +1100

    Inline code from parent class for easier access while refactoring

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -32,8 +32,39 @@ class InvitationsController < Devise::InvitationsController
     end
   end
 
+  # PUT /resource/invitation
   def update
     authorize :invitation
-    super
+
+    raw_invitation_token = update_resource_params[:invitation_token]
+    self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
+
+    yield resource if block_given?
+
+    if invitation_accepted
+      if Devise.allow_insecure_sign_in_after_accept
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message :notice, flash_message if is_flashing_format?
+        sign_in(resource_name, resource)
+        respond_with resource, location: after_accept_path_for(resource)
+      else
+        set_flash_message :notice, :updated_not_active if is_flashing_format?
+        respond_with resource, location: new_session_path(resource_name)
+      end
+    else
+      resource.invitation_token = raw_invitation_token
+      respond_with_navigational(resource) { render :edit }
+    end
+  end
+
+  private
+
+  def accept_resource
+    resource_class.accept_invitation!(update_resource_params)
+  end
+
+  def update_resource_params
+    devise_parameter_sanitizer.sanitize(:accept_invitation)
   end
 end
