09ec9f9d9ef5104c5a49b0d7a1dd07920dc2539c
Matthew Landauer <mlandauer@gmail.com>
2020-10-26 16:53:13 +1100

    Remove more abstraction on controller

--- app/controllers/admins/passwords_controller.rb
+++ app/controllers/admins/passwords_controller.rb
@@ -30,43 +30,33 @@ module Admins
 
     # PUT /resource/password
     def update
-      self.resource = Admin.reset_password_by_token(
+      @admin = Admin.reset_password_by_token(
         reset_password_token: params[:admin][:reset_password_token],
         password: params[:admin][:password]
       )
 
-      if resource.errors.empty?
-        if Devise.sign_in_after_reset_password
-          flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-          set_flash_message!(:notice, flash_message)
-          resource.after_database_authentication
-          sign_in(:admin, resource)
-        else
-          set_flash_message!(:notice, :updated_not_active)
-        end
-        respond_with resource, location: after_resetting_password_path_for(resource)
+      if @admin.errors.empty?
+        flash[:notice] = if @admin.active_for_authentication?
+                           "Your password has been changed successfully. You are now signed in."
+                         else
+                           "Your password has been changed successfully."
+                         end
+        sign_in(:admin, @admin)
+        redirect_to dash_url
       else
-        set_minimum_password_length
-        respond_with resource
+        render :edit
       end
     end
 
     protected
 
-    def after_resetting_password_path_for(resource)
-      Devise.sign_in_after_reset_password ? after_sign_in_path_for(resource) : new_session_path(:admin)
-    end
-
     # Check if a reset_password_token is provided in the request
     def assert_reset_token_passed
       return unless params[:reset_password_token].blank?
 
-      set_flash_message(:alert, :no_token)
-      redirect_to new_session_path(:admin)
-    end
-
-    def translation_scope
-      "devise.passwords"
+      flash[:notice] = "You can't access this page without coming from a password reset email. " \
+                       "If you do come from a password reset email, please make sure you used the full URL provided."
+      redirect_to new_admin_session_url
     end
   end
 end
