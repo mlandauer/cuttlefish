110cc02ad4ec2dd6fb097c27cb7eb1ac247b78d9
Matthew Landauer <mlandauer@gmail.com>
2020-10-29 09:31:59 +1100

    Make sure that graphiql still works if not logged in

--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -3,6 +3,6 @@
 if Rails.env.development?
   GraphiQL::Rails.config.headers["Authorization"] = lambda do |context|
     # GraphiQL will make requests on behalf of the currently logged in admin
-    context.current_admin.api_key
+    context.current_admin&.api_key
   end
 end
