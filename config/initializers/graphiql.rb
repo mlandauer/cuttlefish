606b665a36a48f57428322ab0dc68d9f03ce0d3d
Matthew Landauer <mlandauer@gmail.com>
2020-10-28 17:16:42 +1100

    Now do graphiql requests on behalf of the currently logged in admin

--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  GraphiQL::Rails.config.headers["Authorization"] = lambda do |_context|
-    # GraphiQL will always make requests on behalf of the first admin
-    Admin.first.api_key
+  GraphiQL::Rails.config.headers["Authorization"] = lambda do |context|
+    # GraphiQL will make requests on behalf of the currently logged in admin
+    context.current_admin.api_key
   end
 end
