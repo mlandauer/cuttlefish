9e65d912973a3bb07967d5641d4a2316632794cc
Matthew Landauer <mlandauer@gmail.com>
2020-10-29 12:06:16 +1100

    Remove abstraction in another action

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -20,24 +20,26 @@ module Admins
     def create
       authorize :registration
 
-      self.resource = Admin.new(sign_up_params)
+      # TODO: Put these in a transaction
+      team = Team.create!
+      @admin = Admin.new(
+        name: params[:admin][:name],
+        email: params[:admin][:email],
+        password: params[:admin][:password],
+        team_id: team.id,
+        site_admin: true
+      )
+      @admin.save
 
-      resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
+      if @admin.persisted?
+        flash[:notice] = "Welcome! You have signed up successfully."
+        sign_in(:admin, @admin)
+        redirect_to dash_url
       else
         clean_up_passwords resource
         set_minimum_password_length
-        respond_with resource
+
+        render :new
       end
     end
 
@@ -102,35 +104,10 @@ module Admins
 
     protected
 
-    # Signs in a user on sign up. You can overwrite this method in your own
-    # RegistrationsController.
-    def sign_up(resource_name, resource)
-      sign_in(resource_name, resource)
-    end
-
-    # The path used after sign up. You need to overwrite this method
-    # in your own RegistrationsController.
-    def after_sign_up_path_for(resource)
-      after_sign_in_path_for(resource) if is_navigational_format?
-    end
-
-    # The path used after sign up for inactive accounts. You need to overwrite
-    # this method in your own RegistrationsController.
-    def after_inactive_sign_up_path_for(resource)
-      scope = Devise::Mapping.find_scope!(resource)
-      router_name = Devise.mappings[scope].router_name
-      context = router_name ? send(router_name) : self
-      context.respond_to?(:root_path) ? context.root_path : "/"
-    end
-
     # Authenticates the current scope and gets the current resource from the session.
     def authenticate_scope!
       send(:"authenticate_#{resource_name}!", force: true)
       self.resource = send(:"current_#{resource_name}")
-    end
-
-    def account_update_params
-      devise_parameter_sanitizer.sanitize(:account_update)
     end
 
     def translation_scope
@@ -141,12 +118,6 @@ module Admins
 
     def check_first_user
       redirect_to new_admin_session_url if Admin.first
-    end
-
-    def sign_up_params
-      team = Team.create!
-      devise_parameter_sanitizer.sanitize(:sign_up)
-                                .merge(team_id: team.id, site_admin: true)
     end
   end
 end
