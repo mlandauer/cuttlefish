41d691cc181c45804cfe1540cc0ecb417f97e499
Matthew Landauer <mlandauer@gmail.com>
2020-11-02 16:22:19 +1100

    Remove more abstraction in session controller

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -6,13 +6,10 @@ module Admins
     before_action :check_first_user, only: :new
 
     prepend_before_action :require_no_authentication, only: %i[new create]
-    prepend_before_action :allow_params_authentication!, only: :create
-    prepend_before_action :verify_signed_out_user, only: :destroy
-    prepend_before_action(only: %i[create destroy]) { request.env["devise.skip_timeout"] = true }
 
     # GET /resource/sign_in
     def new
-      @admin = Admin.new(email: params[:admin][:email])
+      @admin = Admin.new(email: params[:admin]&.[](:email))
     end
 
     # POST /resource/sign_in
@@ -24,9 +21,10 @@ module Admins
         # TODO: Empty this when we logout
         session[:jwt_token] = result.data.login_admin.token
         admin = Admin.find(result.data.login_admin.admin.id)
-        set_flash_message!(:notice, :signed_in)
+        flash[:notice] = "Signed in successfully."
         sign_in :admin, admin
-        respond_with admin, location: after_sign_in_path_for(admin)
+
+        redirect_to dash_url
       else
         flash[:alert] = result.data.login_admin.errors.map(&:message).join(", ")
         @admin = AdminForm.new(
@@ -39,63 +37,16 @@ module Admins
 
     # DELETE /resource/sign_out
     def destroy
-      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-      set_flash_message! :notice, :signed_out if signed_out
-      yield if block_given?
-      respond_to_on_destroy
-    end
+      signed_out = sign_out
+      flash[:notice] = "Signed out successfully." if signed_out
 
-    protected
-
-    def sign_in_params
-      devise_parameter_sanitizer.sanitize(:sign_in)
-    end
-
-    def serialize_options(resource)
-      methods = resource_class.authentication_keys.dup
-      methods = methods.keys if methods.is_a?(Hash)
-      methods << :password if resource.respond_to?(:password)
-      { methods: methods, only: [:password] }
-    end
-
-    def auth_options
-      { scope: resource_name, recall: "#{controller_path}#new" }
-    end
-
-    def translation_scope
-      "devise.sessions"
+      redirect_to root_url
     end
 
     private
 
-    # Check if there is no signed in user before doing the sign out.
-    #
-    # If there is no signed in user, it will set the flash message and redirect
-    # to the after_sign_out path.
-    def verify_signed_out_user
-      return unless all_signed_out?
-
-      set_flash_message! :notice, :already_signed_out
-
-      respond_to_on_destroy
-    end
-
-    def all_signed_out?
-      users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
-
-      users.all?(&:blank?)
-    end
-
-    def respond_to_on_destroy
-      # We actually need to hardcode this as Rails default responder doesn't
-      # support returning empty response on GET request
-      respond_to do |format|
-        format.all { head :no_content }
-        format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
-      end
-    end
-
     def check_first_user
+      # TODO: Get this info from the API instead
       redirect_to new_admin_registration_url if Admin.first.nil?
     end
   end
