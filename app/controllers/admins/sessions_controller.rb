c16aeaec8a44599325b8885ddc6c783f411a90d6
Matthew Landauer <mlandauer@gmail.com>
2020-11-02 15:54:05 +1100

    Inline parent class for easy reference

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
 
 module Admins
-  class SessionsController < Devise::SessionsController
+  class SessionsController < DeviseController
     layout "login"
     before_action :check_first_user, only: :new
 
+    prepend_before_action :require_no_authentication, only: [:new, :create]
+    prepend_before_action :allow_params_authentication!, only: :create
+    prepend_before_action :verify_signed_out_user, only: :destroy
+    prepend_before_action(only: [:create, :destroy]) { request.env["devise.skip_timeout"] = true }
+
+    # GET /resource/sign_in
+    def new
+      self.resource = resource_class.new(sign_in_params)
+      clean_up_passwords(resource)
+      yield resource if block_given?
+      respond_with(resource, serialize_options(resource))
+    end
+
+    # POST /resource/sign_in
     def create
       result = api_query email: params[:admin][:email],
                          password: params[:admin][:password]
@@ -26,7 +40,63 @@ module Admins
       end
     end
 
+    # DELETE /resource/sign_out
+    def destroy
+      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      set_flash_message! :notice, :signed_out if signed_out
+      yield if block_given?
+      respond_to_on_destroy
+    end
+
+    protected
+
+    def sign_in_params
+      devise_parameter_sanitizer.sanitize(:sign_in)
+    end
+
+    def serialize_options(resource)
+      methods = resource_class.authentication_keys.dup
+      methods = methods.keys if methods.is_a?(Hash)
+      methods << :password if resource.respond_to?(:password)
+      { methods: methods, only: [:password] }
+    end
+
+    def auth_options
+      { scope: resource_name, recall: "#{controller_path}#new" }
+    end
+
+    def translation_scope
+      'devise.sessions'
+    end
+
     private
+
+    # Check if there is no signed in user before doing the sign out.
+    #
+    # If there is no signed in user, it will set the flash message and redirect
+    # to the after_sign_out path.
+    def verify_signed_out_user
+      if all_signed_out?
+        set_flash_message! :notice, :already_signed_out
+
+        respond_to_on_destroy
+      end
+    end
+
+    def all_signed_out?
+      users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
+
+      users.all?(&:blank?)
+    end
+
+    def respond_to_on_destroy
+      # We actually need to hardcode this as Rails default responder doesn't
+      # support returning empty response on GET request
+      respond_to do |format|
+        format.all { head :no_content }
+        format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+      end
+    end
 
     def check_first_user
       redirect_to new_admin_registration_url if Admin.first.nil?
