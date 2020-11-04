b50abb4c532a7cb0048f08ebf0d410e10aba6e41
Matthew Landauer <mlandauer@gmail.com>
2020-11-04 15:14:53 +1100

    We *do* need to use devise_for
    It does some devise magic that isn't just about generating routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
   post "/graphql", to: "graphql#execute"
+  devise_for :admins, only: []
 
   resource :session,
            only: [],
