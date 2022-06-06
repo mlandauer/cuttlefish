00bc2959d90ebb7cbb46ea98acce2f5157e58dc3
Matthew Landauer <mlandauer@gmail.com>
2022-06-06 15:47:16 +1000

    Service that sets up SSL for the custom domains

---
+++ app/services/app_services/setup_custom_tracking_domain_ssl.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module AppServices
+  # For an app with id "id" and a valid custom_tracking_domain set generate an SSL certificate, setup our web server
+  # use it and let the rest of the application to know to use it too
+  class SetupCustomTrackingDomainSSL < ApplicationService
+    def initialize(id:)
+      super()
+      @id = id
+    end
+
+    def call
+      app = App.find(id)
+      # Double check that we actually do have a custom tracking domain setup
+      if app.custom_tracking_domain.blank?
+        fail!
+        return
+      end
+
+      # This will raise an exception if it fails for some reason
+      Certificate.new(app.custom_tracking_domain).generate
+      # So if we get here that means it worked
+      if app.update(custom_tracking_domain_ssl_enabled: true)
+        success!
+      else
+        fail!
+      end
+    end
+
+    private
+
+    attr_reader :id
+  end
+end
