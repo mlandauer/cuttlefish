089945f7648dcba2c65e5574d2ae55d589e4a664
Matthew Landauer <mlandauer@gmail.com>
2023-08-14 03:39:37 +0000

    Update tests after changing the ssl redirect logic

--- spec/controllers/admins/sessions_controller_spec.rb
+++ spec/controllers/admins/sessions_controller_spec.rb
@@ -7,25 +7,7 @@ describe Admins::SessionsController, type: :controller do
     request.env["devise.mapping"] = Devise.mappings[:admin]
   end
 
-  context "when request is over http" do
-    context "when there is one admin already registered" do
-      before do
-        team = Team.create!
-        team.admins.create!(email: "foo@bar.com", password: "guess this")
-      end
-
-      it "redirects to https" do
-        get :new
-        expect(response).to redirect_to(action: :new, protocol: "https")
-      end
-    end
-  end
-
   context "when request is over https" do
-    before do
-      request.env["HTTPS"] = "on"
-    end
-
     context "when this is a fresh install and there are no admins registered" do
       it "redirects to the registration page" do
         get :new
@@ -39,7 +21,7 @@ describe Admins::SessionsController, type: :controller do
         team.admins.create!(email: "foo@bar.com", password: "guess this")
       end
 
-      it "does not redirect https" do
+      it "does not redirect" do
         get :new
         expect(response).to be_successful
       end
