f0b254db4975c54c490094a129de5da421924f38
Matthew Landauer <mlandauer@gmail.com>
2023-10-20 01:34:18 +0000

    Inline css does not mangle html5 now

--- lib/filters/inline_css.rb
+++ lib/filters/inline_css.rb
@@ -3,12 +3,12 @@
 module Filters
   class InlineCss < Filters::Mail
     def filter_html(input)
-      premailer = Premailer.new(
+      Premailer.new(
         input,
         with_html_string: true,
-        input_encoding: input.encoding.to_s
-      )
-      premailer.to_inline_css
+        input_encoding: input.encoding.to_s,
+        adapter: (html5?(input) ? :nokogumbo : :nokogiri)
+      ).to_inline_css
     end
   end
 end
--- spec/lib/filters/inline_css_spec.rb
+++ spec/lib/filters/inline_css_spec.rb
@@ -3,31 +3,39 @@
 require "spec_helper"
 
 describe Filters::InlineCss do
-  context "with html email with no styling" do
-    let(:html) { "<p>This is HTML with “some” UTF-8</p>" }
+  describe "#filter_html" do
+    context "with html email with no styling" do
+      let(:html) { "<p>This is HTML with “some” UTF-8</p>" }
 
-    it "#filter_html" do
-      expect(described_class.new.filter_html(html)).to eq <<~HTML
-        <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-        <html><body><p>This is HTML with “some” UTF-8</p></body></html>
-      HTML
+      it do
+        expect(described_class.new.filter_html(html)).to eq <<~HTML
+          <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+          <html><body><p>This is HTML with “some” UTF-8</p></body></html>
+        HTML
+      end
+    end
+
+    context "with html email with style block" do
+      let(:html) do
+        "<head><style>p { font-size: 20px; }</style></head>" \
+          "<body><p>This is HTML with “some” UTF-8</p></body>"
+      end
+
+      it do
+        expect(described_class.new.filter_html(html)).to eq <<~HTML
+          <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+          <html>
+          <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head>
+          <body><p style="font-size: 20px;">This is HTML with “some” UTF-8</p></body>
+          </html>
+        HTML
+      end
     end
   end
 
-  context "with html email with style block" do
-    let(:html) do
-      "<head><style>p { font-size: 20px; }</style></head>" \
-        "<body><p>This is HTML with “some” UTF-8</p></body>"
-    end
-
-    it "#filter_html" do
-      expect(described_class.new.filter_html(html)).to eq <<~HTML
-        <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-        <html>
-        <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head>
-        <body><p style="font-size: 20px;">This is HTML with “some” UTF-8</p></body>
-        </html>
-      HTML
-    end
+  it "doesn't mangle valid html 5" do
+    html = '<!DOCTYPE html><a href="#"><table></table></a>'
+    expected = '<!DOCTYPE html><html><head></head><body><a href="#"><table></table></a></body></html>'
+    expect(described_class.new.filter_html(html).gsub("\n","")).to eq(expected)
   end
 end
